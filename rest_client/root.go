21e3a514950cfb0a4c10b8d3fc2456252f7b01cb
vaibhavchellani <vaibhavchellani223@gmail.com>
2018-08-23 23:10:14 +0530

    added rest server and endpoint for submitting transaction

---
+++ rest_client/root.go
@@ -0,0 +1,100 @@
+package rest_client
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/tendermint/tendermint/libs/log"
+	"os"
+	"net/http"
+	"github.com/gorilla/mux"
+	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/wire"
+	cmn "github.com/tendermint/tendermint/libs/common"
+	tmserver "github.com/tendermint/tendermint/rpc/lib/server"
+	"github.com/cosmos/cosmos-sdk/client"
+	auth "github.com/cosmos/cosmos-sdk/x/auth/client/rest"
+	bank "github.com/cosmos/cosmos-sdk/x/bank/client/rest"
+	gov "github.com/cosmos/cosmos-sdk/x/gov/client/rest"
+	ibc "github.com/cosmos/cosmos-sdk/x/ibc/client/rest"
+	slashing "github.com/cosmos/cosmos-sdk/x/slashing/client/rest"
+	stake "github.com/cosmos/cosmos-sdk/x/stake/client/rest"
+	sideblock "github.com/basecoin/sideblock/rest"
+	"github.com/cosmos/cosmos-sdk/client/keys"
+	"github.com/cosmos/cosmos-sdk/client/rpc"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+)
+
+
+// ServeCommand will generate a long-running rest server
+// (aka Light Client Daemon) that exposes functionality similar
+// to the cli, but over rest
+func ServeMaticCommands(cdc *wire.Codec) *cobra.Command {
+	flagListenAddr := "laddr"
+	flagCORS := "cors"
+	flagMaxOpenConnections := "max-open"
+
+	cmd := &cobra.Command{
+		Use:   "rest-server",
+		Short: "Start LCD (light-client daemon), a local REST server",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			listenAddr := viper.GetString(flagListenAddr)
+			handler := createHandler(cdc)
+			logger := log.NewTMLogger(log.NewSyncWriter(os.Stdout)).With("module", "rest-server")
+			maxOpen := viper.GetInt(flagMaxOpenConnections)
+
+			listener, err := tmserver.StartHTTPServer(
+				listenAddr, handler, logger,
+				tmserver.Config{MaxOpenConnections: maxOpen},
+			)
+			if err != nil {
+				return err
+			}
+
+			logger.Info("REST server started")
+
+			// wait forever and cleanup
+			cmn.TrapSignal(func() {
+				err := listener.Close()
+				logger.Error("error closing listener", "err", err)
+			})
+
+			return nil
+		},
+	}
+
+	cmd.Flags().String(flagListenAddr, "tcp://localhost:1317", "The address for the server to listen on")
+	cmd.Flags().String(flagCORS, "", "Set the domains that can make CORS requests (* for all)")
+	cmd.Flags().String(client.FlagChainID, "", "The chain ID to connect to")
+	cmd.Flags().String(client.FlagNode, "tcp://localhost:26657", "Address of the node to connect to")
+	cmd.Flags().Int(flagMaxOpenConnections, 1000, "The number of maximum open connections")
+
+	return cmd
+}
+
+func createHandler(cdc *wire.Codec) http.Handler {
+	r := mux.NewRouter()
+
+	kb, err := keys.GetKeyBase() //XXX
+	if err != nil {
+		panic(err)
+	}
+
+	cliCtx := context.NewCLIContext().WithCodec(cdc).WithLogger(os.Stdout)
+
+	// TODO: make more functional? aka r = keys.RegisterRoutes(r)
+	//r.HandleFunc("/version", CLIVersionRequestHandler).Methods("GET")
+	//r.HandleFunc("/node_version", NodeVersionRequestHandler(cliCtx)).Methods("GET")
+
+	keys.RegisterRoutes(r)
+	rpc.RegisterRoutes(cliCtx, r)
+	tx.RegisterRoutes(cliCtx, r, cdc)
+	auth.RegisterRoutes(cliCtx, r, cdc, "acc")
+	bank.RegisterRoutes(cliCtx, r, cdc, kb)
+	ibc.RegisterRoutes(cliCtx, r, cdc, kb)
+	stake.RegisterRoutes(cliCtx, r, cdc, kb)
+	slashing.RegisterRoutes(cliCtx, r, cdc, kb)
+	gov.RegisterRoutes(cliCtx, r, cdc)
+	// Addded rest commands to adding transction !
+	sideblock.RegisterRoutes(cliCtx,r,cdc,kb)
+	return r
+}
--- sideblock/rest/tx.go
+++ sideblock/rest/tx.go
@@ -6,14 +6,96 @@ import (
 	"github.com/cosmos/cosmos-sdk/wire"
 	"github.com/cosmos/cosmos-sdk/crypto/keys"
 	"net/http"
+	"encoding/json"
+	"io/ioutil"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	authctx "github.com/cosmos/cosmos-sdk/x/auth/client/context"
+	"github.com/basecoin/sideblock"
+	"fmt"
 )
 
 func registerTxRoutes(cliCtx context.CLIContext, r *mux.Router, cdc *wire.Codec, kb keys.Keybase) {
 	r.HandleFunc(
 		"/sideblock/submitBlock",
-		unrevokeRequestHandlerFn(cdc, kb, cliCtx),
+		sideblockRequestHandlerFn(cdc, kb, cliCtx),
 	).Methods("POST")
+
 }
-func unrevokeRequestHandlerFn(codec *wire.Codec, keybase keys.Keybase, cliContext context.CLIContext) func(http.ResponseWriter, *http.Request) {
-	
+type SideBlockBody struct {
+	LocalAccountName string `json:"name"`
+	Password         string `json:"password"`
+	ChainID          string `json:"chain_id"`
+	AccountNumber    int64  `json:"account_number"`
+	Sequence         int64  `json:"sequence"`
+	Gas              int64  `json:"gas"`
+	VariableAddr    string `json:"validator_addr"`
+	BlockHash 		string `json:"blockhash"`
+	TxRoot 			string `json:"tx_root"`
+	ReceiptRoot 	string `json:"receipt_root"`
+}
+func sideblockRequestHandlerFn(cdc *wire.Codec, kb keys.Keybase, cliCtx context.CLIContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var m SideBlockBody
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		err = json.Unmarshal(body, &m)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		//info, err := kb.Get(m.LocalAccountName)
+		//if err != nil {
+		//	w.WriteHeader(http.StatusUnauthorized)
+		//	w.Write([]byte(err.Error()))
+		//	return
+		//}
+
+		txCtx := authctx.TxContext{
+			Codec:         cdc,
+			ChainID:       m.ChainID,
+			AccountNumber: m.AccountNumber,
+			Sequence:      m.Sequence,
+			Gas:           m.Gas,
+		}
+
+		variableAddress, err := sdk.AccAddressFromBech32(m.VariableAddr)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(fmt.Sprintf("Couldn't decode address. Error: %s", err.Error())))
+			return
+		}
+
+		msg := sideBlock.NewMsgSideBlock(variableAddress,string(m.BlockHash),string(m.TxRoot),string(m.ReceiptRoot))
+
+		txBytes, err := txCtx.BuildAndSign(m.LocalAccountName, m.Password, []sdk.Msg{msg})
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		res, err := cliCtx.BroadcastTx(txBytes)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		output, err := json.MarshalIndent(res, "", "  ")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		w.Write(output)
+
+	}
 }
