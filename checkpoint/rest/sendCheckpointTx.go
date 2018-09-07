90b02e1df6b435c939eba0413f9521556bade0f7
vaibhavchellani <vaibhavchellani223@gmail.com>
2018-09-07 19:35:54 +0530

    solved the struct populating issue

--- checkpoint/rest/sendCheckpointTx.go
+++ checkpoint/rest/sendCheckpointTx.go
@@ -24,15 +24,15 @@ func registerTxRoutes(cliCtx context.CLIContext, r *mux.Router, cdc *wire.Codec,
 }
 type CheckpointBody struct {
 	Password         string `json:"password"`
-	LocalAccountName string `json:"local_account_name"`
-	ChainID          string `json:"chain_id"`
-	AccountNumber    int64  `json:"account_number"`
+	Local_account_name string `json:"local_account_name"`
+	Chain_id          string `json:"chain_id"`
+	Account_number    int64  `json:"account_number"`
 	Sequence         int64  `json:"sequence"`
 	Gas              int64  `json:"gas"`
-	RootHash    	 string `json:"root_hash"`
-	StartBlock 		 int64 `json:"start_block"`
-	EndBlock 		 int64 `json:"end_block"`
-	ProposerAddress  string `json:"proposer"`
+	Root_hash    	 string `json:"root_hash"`
+	Start_block 		 int64 `json:"start_block"`
+	End_block 		 int64 `json:"end_block"`
+	Proposer_address  string `json:"proposer_address"`
 }
 func submitCheckpointRequestHandlerFn(cdc *wire.Codec, kb keys.Keybase, cliCtx context.CLIContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -52,33 +52,36 @@ func submitCheckpointRequestHandlerFn(cdc *wire.Codec, kb keys.Keybase, cliCtx c
 			w.Write([]byte(err.Error()))
 			return
 		}
+		fmt.Println("--------")
+		fmt.Printf("the body is \n and %v",m)
+		fmt.Println("--------")
 
-		info, err := kb.Get("alice")
-		if err != nil {
-			w.WriteHeader(http.StatusUnauthorized)
-			w.Write([]byte(err.Error()))
-			return
-		}
+		//info, err := kb.Get(m.Local_account_name)
+		//if err != nil {
+		//	w.WriteHeader(http.StatusUnauthorized)
+		//	w.Write([]byte(err.Error()))
+		//	return
+		//}
 
 		txCtx := authctx.TxContext{
 			Codec:         cdc,
-			ChainID:       "test-chain-ZLAVft",
-			AccountNumber: 0,
-			Sequence:      1,
-			Gas:           1000000,
+			ChainID:       m.Chain_id,
+			AccountNumber: m.Account_number,
+			Sequence:      m.Sequence,
+			Gas:           m.Gas,
 		}
 
-		proposerAddress, err := sdk.AccAddressFromBech32("cosmosaccaddr15epdnu350uzfjau26scvazux4yj5mpag2ag6us")
+		proposerAddress, err := sdk.AccAddressFromBech32(m.Proposer_address)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(fmt.Sprintf("Couldn't decode address. Error: %s", err.Error())))
 			return
 		}
-		fmt.Printf( "vairable address is %v  \n",proposerAddress)
-		fmt.Printf( "tx ctx is  is %v  \n",txCtx)
+		//fmt.Printf( "vairable address is %v  \n",proposerAddress)
+		//fmt.Printf( "tx ctx is  is %v  \n",txCtx)
 
-		msg := checkpoint.NewMsgCheckpointBlock	(sdk.AccAddress(proposerAddress),12,34,"vaibhavroothash")
-		txBytes, err := txCtx.BuildAndSign("alice", "password", []sdk.Msg{msg})
+		msg := checkpoint.NewMsgCheckpointBlock	(sdk.AccAddress(proposerAddress),int(m.Start_block),int(m.End_block),m.Root_hash)
+		txBytes, err := txCtx.BuildAndSign(m.Local_account_name, m.Password, []sdk.Msg{msg})
 		if err != nil {
 			w.WriteHeader(http.StatusUnauthorized)
 			w.Write([]byte(err.Error()))
@@ -86,23 +89,31 @@ func submitCheckpointRequestHandlerFn(cdc *wire.Codec, kb keys.Keybase, cliCtx c
 		}
 		fmt.Printf("txbytes are %v \n",txBytes)
 		//
+
+
+		//TODO uncomment to send transaction
 		res, err := cliCtx.BroadcastTx(txBytes)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(err.Error()))
 			return
 		}
-		fmt.Printf("response is are %v \n",res)
 
-		fmt.Printf("info is %v /n",info)
+
+
+		//fmt.Printf("response is are %v \n",res)
+
+		//fmt.Printf("info is %v /n",info)
+
+		// TODO uncomment
 		output, err := json.MarshalIndent(res, "", "  ")
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(err.Error()))
 			return
 		}
-		fmt.Printf("yay output is %v",output)
-		//
+		//fmt.Printf("yay output is %v",output)
+		//TODO this too
 		w.Write(output)
 
 	}
