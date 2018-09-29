6dd656896b50a28a79e7fe92150690dfff8b0ec6
vaibhavchellani <vaibhavchellani223@gmail.com>
2018-09-29 18:15:28 +0530

    connect to kovan and sample contract && peppermint

--- trial/main2.go
+++ trial/main2.go
@@ -1,28 +1,34 @@
 package main
+
 import (
-"encoding/hex"
-"fmt"
-"github.com/ethereum/go-ethereum/crypto/sha3"
-)
+	"github.com/tendermint/tendermint/privval"
+	"fmt"
+	"encoding/hex"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"log"
+	"github.com/ethereum/go-ethereum/common"
+	contracts "../contracts"
+	)
 
 func main() {
-hash := sha3.NewKeccak256()
-
-var buf []byte
-//hash.Write([]byte{0xcc})
-fmt.Printf("cc after decode %v",decodeHex("cc"))
-hash.Write(decodeHex("cc"))
-buf = hash.Sum(buf)
-
-fmt.Println(hex.EncodeToString(buf))
-//expected := "EEAD6DBFC7340A56CAEDC044696A168870549A6A7F6F56961E84A54BD9970B8A"
-}
-
-func decodeHex(s string) []byte {
-b, err := hex.DecodeString(s)
-if err != nil {
-panic(err)
-}
-
-return b
-}
+	//privVal := privval.LoadFilePV(config.DefaultBaseConfig().PrivValidatorFile())
+	privValObj := privval.LoadFilePV("/Users/vc/.basecoind/config/priv_validator.json")
+	sig,err:=privValObj.PrivKey.Sign(crypto.Keccak256([]byte("hello")))
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Signature is %v",hex.EncodeToString(sig))
+	client, err := ethclient.Dial("https://kovan.infura.io/TJSJL5u9maRXnaZrSvnv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("*** WE are connected to kovan ******")
+	contractAddress  := common.HexToAddress("0xd8854cc230a393b31d7f0ef99b4dea14b91a5b41")
+	instance,err := contracts.NewContract(contractAddress,client)
+	if err!=nil {
+		fmt.Println("error error")
+	}
+	fmt.Println(instance)
+	
+	}
