53504b829f45ea422de3090d16ff40ef9bd593b5
vaibhavchellani <vaibhavchellani223@gmail.com>
2018-09-30 22:56:06 +0530

    formatted

--- trial/main.go
+++ trial/main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"context"
-	"math/big"
-	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/ethereum/go-ethereum/crypto/sha3"
 	"encoding/hex"
+	"fmt"
+	"github.com/ethereum/go-ethereum/crypto/sha3"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"log"
+	"math/big"
 
 	"github.com/xsleonard/go-merkle"
 )
 
-func main()  {
+func main() {
 	client, err := ethclient.Dial("https://testnet.matic.network")
 	if err != nil {
 		log.Fatal(err)
@@ -21,45 +21,45 @@ func main()  {
 	fmt.Println("we have a connection")
 	_ = client // we'll use this in the upcoming sections
 	//TODO reject transaction if the difference in numbers is not even
-	root:=getHeaders(4733028,4733032,client)
-	fmt.Printf("the root hash is %v",root)
+	root := getHeaders(4733028, 4733032, client)
+	fmt.Printf("the root hash is %v", root)
 
 }
 
-func getHeaders(start int,end int,client *ethclient.Client) string {
-	if start>end{
+func getHeaders(start int, end int, client *ethclient.Client) string {
+	if start > end {
 		return ""
 	}
-	fmt.Printf("start from %v /n end to %v",start,end)
-	current:=start
+	fmt.Printf("start from %v /n end to %v", start, end)
+	current := start
 	var result [][32]byte
 	for current <= end {
 		//TODO run this in different goroutines and use channels to fetch results(how to maintian order)
-		blockheader,err:=client.HeaderByNumber(context.Background(),big.NewInt(int64(current)))
-		if err!=nil {
+		blockheader, err := client.HeaderByNumber(context.Background(), big.NewInt(int64(current)))
+		if err != nil {
 			fmt.Printf("not found")
 			log.Fatal(err)
 		}
-		headerBytes := appendBytes32(	blockheader.Number.Bytes(),
-										blockheader.Time.Bytes(),
-										blockheader.TxHash.Bytes(),
-										blockheader.ReceiptHash.Bytes() )
+		headerBytes := appendBytes32(blockheader.Number.Bytes(),
+			blockheader.Time.Bytes(),
+			blockheader.TxHash.Bytes(),
+			blockheader.ReceiptHash.Bytes())
 
-		header:= getsha3frombyte(headerBytes)
+		header := getsha3frombyte(headerBytes)
 		var arr [32]byte
 		copy(arr[:], header)
 		result = append(result, arr)
 		current++
 	}
 	fmt.Println("------")
-	merkelData:=convert(result)
+	merkelData := convert(result)
 	//fmt.Println("------")
-	fmt.Printf("merkel data is \n %v" ,len(merkelData))
+	fmt.Printf("merkel data is \n %v", len(merkelData))
 	fmt.Println("------")
 	//tree := merkle.NewTree()
-	tree := merkle.NewTreeWithOpts(merkle.TreeOptions{EnableHashSorting:false,DisableHashLeaves:true})
+	tree := merkle.NewTreeWithOpts(merkle.TreeOptions{EnableHashSorting: false, DisableHashLeaves: true})
 
-	err := tree.Generate(merkelData,sha3.NewKeccak256())
+	err := tree.Generate(merkelData, sha3.NewKeccak256())
 	if err != nil {
 		fmt.Println("*********ERROR***********")
 		log.Fatal(err)
@@ -72,9 +72,9 @@ func getHeaders(start int,end int,client *ethclient.Client) string {
 }
 func convert(input []([32]byte)) [][]byte {
 	var output [][]byte
-	for _,in := range input{
+	for _, in := range input {
 		newInput := make([]byte, len(in[:]))
-		copy(newInput,in[:])
+		copy(newInput, in[:])
 		output = append(output, newInput)
 		//fmt.Printf("------- \n input is %v \n output is %v",newInput,output )
 
@@ -91,7 +91,7 @@ func convertTo32(input []byte) (output [32]byte, err error) {
 	return
 }
 
-func appendBytes32(data... []byte) []byte {
+func appendBytes32(data ...[]byte) []byte {
 	var result []byte
 	for _, v := range data {
 		paddedV, err := convertTo32(v)
@@ -101,7 +101,7 @@ func appendBytes32(data... []byte) []byte {
 	}
 	return result
 }
-func getsha3frombyte(input []byte) []byte{
+func getsha3frombyte(input []byte) []byte {
 	hash := sha3.NewKeccak256()
 	//v,err:=hex.DecodeString(input)
 	//if err!=nil{
@@ -113,4 +113,3 @@ func getsha3frombyte(input []byte) []byte{
 	buf = hash.Sum(buf)
 	return buf
 }
-
--- trial/main2.go
+++ trial/main2.go
@@ -1,33 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"github.com/ethereum/go-ethereum/ethclient"
-	"log"
-	"github.com/ethereum/go-ethereum/common"
 	contracts "../contracts"
-	"github.com/tendermint/tendermint/privval"
-	"github.com/ethereum/go-ethereum/crypto"
-	"encoding/hex"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"context"
-	"math/big"
-	"github.com/ethereum/go-ethereum/crypto/sha3"
+	"encoding/hex"
+	"fmt"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/crypto/sha3"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/tendermint/tendermint/privval"
+	"log"
+	"math/big"
 )
 
 func main() {
-
 
 	client, err := ethclient.Dial("https://kovan.infura.io/TJSJL5u9maRXnaZrSvnv")
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("\n *** WE are connected to kovan ****** ")
-	contractAddress  := common.HexToAddress("0xd8854cc230a393b31d7f0ef99b4dea14b91a5b41")
-	instance,err := contracts.NewContract(contractAddress,client)
-	if err!=nil {
+	contractAddress := common.HexToAddress("0xd8854cc230a393b31d7f0ef99b4dea14b91a5b41")
+	instance, err := contracts.NewContract(contractAddress, client)
+	if err != nil {
 		fmt.Println("error error")
 	}
 	fmt.Println(instance)
@@ -35,23 +34,22 @@ func main() {
 	privValObj := privval.LoadFilePV("/Users/vc/.basecoind/config/priv_validator.json")
 	pkBytes := privValObj.PrivKey.Bytes()
 
-	fmt.Printf("Public key is :%v\n",hex.EncodeToString(privValObj.PubKey.Address()))
-	fmt.Printf("Private key is : %v\n",hex.EncodeToString(pkBytes[len(pkBytes)-32:]))
+	fmt.Printf("Public key is :%v\n", hex.EncodeToString(privValObj.PubKey.Address()))
+	fmt.Printf("Private key is : %v\n", hex.EncodeToString(pkBytes[len(pkBytes)-32:]))
 
 	privateKey, err := crypto.ToECDSA(pkBytes[len(pkBytes)-32:])
 	if err != nil {
 		panic(err)
 	}
 
-
 	fromAddress := common.BytesToAddress(privValObj.Address)
 
 	auth := bind.NewKeyedTransactor(privateKey)
-	gasprice ,err:= client.SuggestGasPrice(context.Background())
-	if err!=nil {
+	gasprice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
 		fmt.Errorf("Unable to estimate gas")
 	}
-	nonce,err:= client.PendingNonceAt(context.Background(),fromAddress)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
 	if err != nil {
 		panic(err)
 	}
@@ -61,12 +59,12 @@ func main() {
 	methodID := hash.Sum(nil)[:4]
 	fmt.Printf("Method ID: %s\n", hexutil.Encode(methodID))
 
-	auth.Nonce=big.NewInt(int64(nonce))
+	auth.Nonce = big.NewInt(int64(nonce))
 	auth.Value = big.NewInt(0)
 	auth.GasLimit = uint64(300000)
 	auth.GasPrice = gasprice
 	var data []byte
-	hello:=[]byte("vaibhav")
+	hello := []byte("vaibhav")
 	paddedData := common.LeftPadBytes(hello, 32)
 	data = append(data, methodID...)
 	data = append(data, paddedData...)
@@ -82,5 +80,4 @@ func main() {
 
 	fmt.Printf("Transaction sent with Tx Hash : %s", signedTx.Hash().Hex())
 
-	
-	}
+}
