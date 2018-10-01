54827694c4d3dd1dc2811489a81bb0db84c4d75b
vaibhavchellani <vaibhavchellani223@gmail.com>
2018-10-01 12:57:57 +0530

    integrated JD web3 example

--- trial/main2.go
+++ trial/main2.go
@@ -1,83 +1,89 @@
 package main
 
 import (
-	contracts "../contracts"
 	"context"
-	"encoding/hex"
 	"fmt"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/crypto/sha3"
-	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/tendermint/tendermint/privval"
 	"log"
 	"math/big"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	amino "github.com/tendermint/go-amino"
+	"github.com/tendermint/tendermint/crypto/secp256k1"
+	"github.com/tendermint/tendermint/privval"
+
+	"github.com/go-web3-example/contracts/rootchain"
 )
 
-func main() {
+var cdc = amino.NewCodec()
 
-	client, err := ethclient.Dial("https://kovan.infura.io/TJSJL5u9maRXnaZrSvnv")
+func init() {
+	cdc.RegisterConcrete(secp256k1.PubKeySecp256k1{}, secp256k1.Secp256k1PubKeyAminoRoute, nil)
+	cdc.RegisterConcrete(secp256k1.PrivKeySecp256k1{}, secp256k1.Secp256k1PrivKeyAminoRoute, nil)
+}
+
+func main() {
+	client, err := ethclient.Dial("https://kovan.infura.io")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("\n *** WE are connected to kovan ****** ")
-	contractAddress := common.HexToAddress("0xd8854cc230a393b31d7f0ef99b4dea14b91a5b41")
-	instance, err := contracts.NewContract(contractAddress, client)
-	if err != nil {
-		fmt.Println("error error")
-	}
-	fmt.Println(instance)
 
-	privValObj := privval.LoadFilePV("/Users/vc/.basecoind/config/priv_validator.json")
-	pkBytes := privValObj.PrivKey.Bytes()
+	// with no 0x
+	rootchainAddress := "24e01716a6ac34d5f2c4c082f553d86a557543a7"
 
-	fmt.Printf("Public key is :%v\n", hex.EncodeToString(privValObj.PubKey.Address()))
-	fmt.Printf("Private key is : %v\n", hex.EncodeToString(pkBytes[len(pkBytes)-32:]))
+	// token address
+	tokenAddress := "670568761764f53E6C10cd63b71024c31551c9EC"
 
-	privateKey, err := crypto.ToECDSA(pkBytes[len(pkBytes)-32:])
+	// root chain client
+	rootchainClient, err := rootchain.NewRootchain(common.HexToAddress(rootchainAddress), client)
 	if err != nil {
 		panic(err)
 	}
 
-	fromAddress := common.BytesToAddress(privValObj.Address)
+	// load default base config
+	privVal := privval.LoadFilePV("/Users/vc/.basecoind/config/priv_validator.json")
 
-	auth := bind.NewKeyedTransactor(privateKey)
+	// retrieve private key
+	var pkObject secp256k1.PrivKeySecp256k1
+	cdc.MustUnmarshalBinaryBare(privVal.PrivKey.Bytes(), &pkObject)
+
+	// create ecdsa private key
+	ecdsaPrivateKey, err := crypto.ToECDSA(pkObject[:])
+	if err != nil {
+		panic(err)
+	}
+
+	// from address
+	fromAddress := common.BytesToAddress(privVal.Address)
+
+	// fetch gas price
 	gasprice, err := client.SuggestGasPrice(context.Background())
 	if err != nil {
-		fmt.Errorf("Unable to estimate gas")
+		panic(err)
 	}
+
+	// fetch nonce
 	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
 	if err != nil {
 		panic(err)
 	}
-	transferFnSignature := []byte("setHello(string)")
-	hash := sha3.NewKeccak256()
-	hash.Write(transferFnSignature)
-	methodID := hash.Sum(nil)[:4]
-	fmt.Printf("Method ID: %s\n", hexutil.Encode(methodID))
 
+	// create auth
+	auth := bind.NewKeyedTransactor(ecdsaPrivateKey)
+	auth.GasPrice = gasprice
 	auth.Nonce = big.NewInt(int64(nonce))
 	auth.Value = big.NewInt(0)
 	auth.GasLimit = uint64(300000)
-	auth.GasPrice = gasprice
-	var data []byte
-	hello := []byte("vaibhav")
-	paddedData := common.LeftPadBytes(hello, 32)
-	data = append(data, methodID...)
-	data = append(data, paddedData...)
-	tx := types.NewTransaction(nonce, contractAddress, big.NewInt(0), auth.GasLimit, auth.GasPrice, data)
-	signedTx, err := types.SignTx(tx, types.HomesteadSigner{}, privateKey)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = client.SendTransaction(context.Background(), signedTx)
+
+	// Calling contract method
+	var amount big.Int
+	amount.SetUint64(0)
+	tx, err := rootchainClient.Deposit(auth, common.HexToAddress(tokenAddress), common.BytesToAddress(privVal.Address.Bytes()), &amount)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Transaction sent with Tx Hash : %s", signedTx.Hash().Hex())
-
+	fmt.Printf("Pending TX: 0x%x\n", tx.Hash())
 }
