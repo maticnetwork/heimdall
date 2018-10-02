c0cacbb6cd95d6575b300c4af4d57abe0f152017
vaibhavchellani <vaibhavchellani223@gmail.com>
2018-10-02 19:08:50 +0530

    added validator addition to kvstore and getting all validators

--- staker/keeper.go
+++ staker/keeper.go
@@ -2,6 +2,9 @@ package staker
 
 import (sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/wire"
+	abci "github.com/tendermint/tendermint/abci/types"
+
+	"fmt"
 )
 
 type Keeper struct {
@@ -13,6 +16,9 @@ type Keeper struct {
 	codespace sdk.CodespaceType
 }
 
+var (
+	ValidatorsKey                    = []byte{0x02} // prefix for each key to a validator
+)
 
 func NewKeeper(cdc *wire.Codec, key sdk.StoreKey, codespace sdk.CodespaceType) Keeper {
 	keeper := Keeper{
@@ -23,3 +29,46 @@ func NewKeeper(cdc *wire.Codec, key sdk.StoreKey, codespace sdk.CodespaceType) K
 	return keeper
 }
 
+//validator type will contain address, pubkey and power
+func (k Keeper) SetValidatorSet(ctx sdk.Context) {
+	store := ctx.KVStore(k.storeKey)
+	val := abci.Validator{
+		Address:[]byte("dsd"),
+		Power:int64(1),
+
+	}
+	bz,err := k.cdc.MarshalBinary(val)
+	if err!=nil {
+		fmt.Println("error %v",err)
+	}
+	store.Set(GetValidatorKey(val.Address), bz)
+
+}
+func GetValidatorKey(address []byte) []byte {
+	return append(ValidatorsKey,address...)
+}
+
+func (k Keeper)GetAllValidators(ctx sdk.Context) (validators []abci.Validator){
+	store := ctx.KVStore(k.storeKey)
+	iterator := sdk.KVStorePrefixIterator(store, ValidatorsKey)
+
+	i := 0
+	for ; ; i++ {
+		if !iterator.Valid() {
+			break
+		}
+		addr := iterator.Key()[1:]
+		//validator := types.MustUnmarshalValidator(k.cdc, addr, iterator.Value())
+		var validator abci.Validator
+		err := k.cdc.UnmarshalBinary(iterator.Value(), &validator)
+		if err != nil {
+			return
+		}
+		validator.Address=addr
+
+		validators = append(validators, validator)
+		iterator.Next()
+	}
+	iterator.Close()
+	return validators
+}
