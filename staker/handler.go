692c9e4506577b0ea501c7c145e5a547a85e3d01
vaibhavchellani <vaibhavchellani223@gmail.com>
2018-10-09 14:02:22 +0530

    added get validator info

--- staker/handler.go
+++ staker/handler.go
@@ -2,10 +2,12 @@ package staker
 
 import (
 	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	abci "github.com/tendermint/tendermint/abci/types"
 
 	"encoding/hex"
+
 	"github.com/tendermint/tendermint/crypto/secp256k1"
 	tmtypes "github.com/tendermint/tendermint/types"
 )
@@ -32,18 +34,16 @@ func EndBlocker(ctx sdk.Context, k Keeper) (validators []abci.Validator) {
 	_pub, _ := hex.DecodeString("041FE1CDE7D9D8C9182AC967EC8362262216FF8A10061F0DE0F1472F9E45F965D0909DE527E18C7BFB9FCD42335E60FB6E18367A4DC37F1A7FC3265C7241597973")
 	copy(_pubkey[:], _pub[:])
 	_address, _ := hex.DecodeString("F6CEBE8030E5F7F7ED4ADAD55040EE9FDA382EF1")
-
+	validator := k.GetValidatorInfo(ctx, _address)
 	val1 := abci.Validator{
 		Address: _address,
-		Power:   int64(1),
+		Power:   (validator.Power) + 1,
 		PubKey:  tmtypes.TM2PB.PubKey(_pubkey),
 	}
 	validatorSet := []abci.Validator{val1}
 
 	fmt.Printf("all validators are %v \n", k.GetAllValidators(ctx))
 	k.SetValidatorSet(ctx, validatorSet)
-	fmt.Printf("all validators are %v \n", k.GetAllValidators(ctx))
 	k.FlushValidatorSet(ctx)
-	fmt.Printf("all validators are %v \n", k.GetAllValidators(ctx))
 	return validatorSet
 }
--- staker/keeper.go
+++ staker/keeper.go
@@ -1,6 +1,7 @@
 package staker
 
-import (sdk "github.com/cosmos/cosmos-sdk/types"
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/wire"
 	abci "github.com/tendermint/tendermint/abci/types"
 
@@ -8,8 +9,8 @@ import (sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 type Keeper struct {
-	storeKey     sdk.StoreKey
-	cdc          *wire.Codec
+	storeKey sdk.StoreKey
+	cdc      *wire.Codec
 	//validatorSet sdk.ValidatorSet
 
 	// codespace
@@ -17,37 +18,40 @@ type Keeper struct {
 }
 
 var (
-	ValidatorsKey                    = []byte{0x02} // prefix for each key to a validator
+	ValidatorsKey = []byte{0x02} // prefix for each key to a validator
 )
 
 func NewKeeper(cdc *wire.Codec, key sdk.StoreKey, codespace sdk.CodespaceType) Keeper {
 	keeper := Keeper{
-		storeKey:   key,
-		cdc:        cdc,
-		codespace:  codespace,
+		storeKey:  key,
+		cdc:       cdc,
+		codespace: codespace,
 	}
 	return keeper
 }
 
 //validator type will contain address, pubkey and power
-func (k Keeper) SetValidatorSet(ctx sdk.Context,validators []abci.Validator) {
+func (k Keeper) SetValidatorSet(ctx sdk.Context, validators []abci.Validator) {
 	store := ctx.KVStore(k.storeKey)
 
 	for _, validator := range validators {
-		bz,err := k.cdc.MarshalBinary(validator)
-		if err!=nil {
-			fmt.Println("error %v",err)
+		bz, err := k.cdc.MarshalBinary(validator)
+		if err != nil {
+			fmt.Println("error %v", err)
 		}
 		store.Set(GetValidatorKey(validator.Address), bz)
 
 	}
 
 }
+
+// appends the validator key to address
 func GetValidatorKey(address []byte) []byte {
-	return append(ValidatorsKey,address...)
+	return append(ValidatorsKey, address...)
 }
 
-func (k Keeper) GetAllValidators(ctx sdk.Context) (validators []abci.Validator){
+// returns all validators added for a specific validafor key
+func (k Keeper) GetAllValidators(ctx sdk.Context) (validators []abci.Validator) {
 	store := ctx.KVStore(k.storeKey)
 	iterator := sdk.KVStorePrefixIterator(store, ValidatorsKey)
 
@@ -65,7 +69,7 @@ func (k Keeper) GetAllValidators(ctx sdk.Context) (validators []abci.Validator){
 		if err != nil {
 			return
 		}
-		validator.Address=addr
+		validator.Address = addr
 
 		validators = append(validators, validator)
 		iterator.Next()
@@ -74,6 +78,15 @@ func (k Keeper) GetAllValidators(ctx sdk.Context) (validators []abci.Validator){
 	return validators
 }
 
+// given the address returns validator info
+func (k Keeper) GetValidatorInfo(ctx sdk.Context, address []byte) (validator abci.Validator) {
+	store := ctx.KVStore(k.storeKey)
+	validator := store.Get(GetValidatorKey(address))
+	return validator
+
+}
+
+// flushes the whole validator set
 func (k Keeper) FlushValidatorSet(ctx sdk.Context) {
 	store := ctx.KVStore(k.storeKey)
 	iterator := sdk.KVStorePrefixIterator(store, ValidatorsKey)
@@ -89,11 +102,11 @@ func (k Keeper) FlushValidatorSet(ctx sdk.Context) {
 		if err != nil {
 			return
 		}
-		validator.Address=addr
-		validator.Power=int64(0)
-		bz,err := k.cdc.MarshalBinary(validator)
-		if err!=nil {
-			fmt.Println("error %v",err)
+		validator.Address = addr
+		validator.Power = int64(0)
+		bz, err := k.cdc.MarshalBinary(validator)
+		if err != nil {
+			fmt.Println("error %v", err)
 		}
 		store.Set(GetValidatorKey(validator.Address), bz)
 		iterator.Next()
