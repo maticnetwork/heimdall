cbe729fe84e6965da80dd9900b0ac26dccdf3cd8
Jaynti Kanani <jdkanani@gmail.com>
2018-11-01 10:55:14 +0530

    change name to GetValidators

--- helper/call.go
+++ helper/call.go
@@ -10,46 +10,7 @@ import (
 	tmtypes "github.com/tendermint/tendermint/types"
 )
 
-//func GetValidators() (validators []abci.Validator) {
-//	stakeManagerInstance, err := GetStakeManagerInstance()
-//	if err!=nil{
-//		Logger.Error("Error creating stakeManagerInstance","Error",err)
-//	}
-//
-//	ValidatorAddrs, err := stakeManagerInstance.GetCurrentValidatorSet(nil)
-//	if err != nil {
-//		Logger.Info("Error getting validator set", "Error", err)
-//	}
-//
-//	for index := range ValidatorAddrs {
-//		if ValidatorAddrs[index].String() != "" {
-//			validatorStruct, error := stakeManagerInstance.Stakers(nil, ValidatorAddrs[index])
-//			if error != nil {
-//				Logger.Error("Error Fetching Staker", "Error", error, "Index", index)
-//			}
-//			pubkey := validatorStruct.Pubkey
-//			var pubkeyBytes secp256k1.PubKeySecp256k1
-//			_pubkey, _ := hex.DecodeString(pubkey)
-//			copy(pubkeyBytes[:], _pubkey)
-//
-//			// todo add a check to check pubkey corresponds to address
-//			validator := abci.Validator{
-//				Address: ValidatorAddrs[index].Bytes(),
-//				Power:   validatorStruct.Amount.Int64(),
-//				PubKey:  tmtypes.TM2PB.PubKey(pubkeyBytes),
-//			}
-//
-//
-//			validators = append(validators, validator)
-//		} else {
-//			Logger.Info("Validator Empty", "Index", index)
-//		}
-//	}
-//
-//	return validators
-//}
-
-func GetValidatorsFromMock() (validators []abci.Validator) {
+func GetValidators() (validators []abci.Validator) {
 	stakeManagerInstance, err := GetStakeManagerInstance()
 	if err != nil {
 		Logger.Error("Error creating validatorSetInstance", "error", err)
