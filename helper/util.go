adc4a287695f71b05509d1f0a1fb704c084647ba
vaibhavchellani <vaibhavchellani223@gmail.com>
2018-11-05 12:53:41 +0530

    added helper/utils

---
+++ helper/util.go
@@ -0,0 +1,10 @@
+package helper
+import(
+	abci "github.com/tendermint/tendermint/abci/types"
+)
+func ValidatorsToString(validators []abci.Validator)(validatorStr []string){
+	for index , validator := range(validators){
+		validatorStr[index]=validator.String()
+	}
+	return
+}
