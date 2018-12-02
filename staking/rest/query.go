3b1cbeea43494d728a862d8de9d00a9736f73a4e
vaibhavchellani <vaibhavchellani223@gmail.com>
2018-12-02 18:49:39 +0530

    added times and loop for proposer getter

--- staking/rest/query.go
+++ staking/rest/query.go
@@ -23,6 +23,10 @@ func registerQueryRoutes(cliCtx context.CLIContext, r *mux.Router, cdc *codec.Co
 		"/staking/validatorSet",
 		ValidatorSetHandlerFn(cdc, cliCtx),
 	).Methods("GET")
+	r.HandleFunc(
+		"/staking/proposer/{times}",
+		ProposerHandlerFn(cdc, cliCtx),
+	).Methods("GET")
 
 }
 
@@ -86,7 +90,54 @@ func ValidatorSetHandlerFn(
 		var _validatorSet tmTypes.ValidatorSet
 		cdc.UnmarshalBinary(res, &_validatorSet)
 
+		// todo format validator set to remove pubkey like we did for validator
 		result, err := json.Marshal(&_validatorSet)
+		if err != nil {
+			RestLogger.Error("Error while marshalling resposne to Json", "error", err)
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(result)
+
+	}
+}
+
+// get proposer for current validator set
+func ProposerHandlerFn(
+	cdc *codec.Codec,
+	cliCtx context.CLIContext,
+) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		times := common.HexToAddress(vars["times"])
+
+		res, err := cliCtx.QueryStore(hmcommon.CurrentValidatorSetKey, "staker")
+		if err != nil {
+			utils.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		// the query will return empty if there is no data
+		if len(res) == 0 {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		var _validatorSet tmTypes.ValidatorSet
+		cdc.UnmarshalBinary(res, &_validatorSet)
+
+		var proposers []tmTypes.Validator
+
+		for index := range times {
+			proposers[index] = *_validatorSet.Proposer
+			_validatorSet.IncrementAccum(1)
+		}
+
+		// todo format validator set to remove pubkey like we did for validator
+		result, err := json.Marshal(&proposers)
 		if err != nil {
 			RestLogger.Error("Error while marshalling resposne to Json", "error", err)
 			w.WriteHeader(http.StatusBadRequest)
