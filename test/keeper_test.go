5fa87394ed325148192b115fbb26b32bfc3522eb
vaibhavchellani <vaibhavchellani223@gmail.com>
2018-12-08 19:28:50 +0530

    added keeper test

---
+++ test/keeper_test.go
@@ -0,0 +1,13 @@
+package test
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestUpdateAck(t *testing.T) {
+	ctx, keeper := CreateTestInput(t, false)
+	keeper.UpdateACKCount(ctx)
+	ack := keeper.GetACKCount(ctx)
+	require.Equal(t, uint64(0), ack, "Ack Count Not Equal")
+}
