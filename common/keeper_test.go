184169821b01d5eac4c2883350198c884154ab1b
vaibhavchellani <vaibhavchellani223@gmail.com>
2018-12-10 19:22:16 +0530

    fixed conflicts

---
+++ common/keeper_test.go
@@ -0,0 +1,17 @@
+package common
+
+//func MakeTestPulp() *hmTypes.Pulp {
+//	pulp := hmTypes.GetPulpInstance()
+//
+//	// register custom type
+//	checkpoint.RegisterPulp(pulp)
+//	staking.RegisterPulp(pulp)
+//
+//	return pulp
+//}
+//
+//func TestUpdateACKCount(t *testing.T) {
+//	ctx, keeper := tests.CreateTestInput(t, false)
+//	ack := keeper.GetACKCount(ctx)
+//	require.Equal(t, 0, ack, "Ack Count Not Equal")
+//}
