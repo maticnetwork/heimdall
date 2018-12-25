c8f5447b2e3e77f7fb60f5b1e75bae4f0c54af4b
vaibhavchellani <vaibhavchellani223@gmail.com>
2018-12-25 13:07:38 +0530

    added checkpoint handler test

--- test/keeper_test.go
+++ test/keeper_test.go
@@ -266,4 +266,23 @@ type MockHeimdallCaller interface {
 	//GetBalance(address common.Address) (*big.Int, error)
 }
 
+//
+//func TestHandleMsgCheckpoint(t *testing.T){
+//	ctx,keeper:=CreateTestInput(t,false)
+//	header,err:=GenRandCheckpointHeader()
+//	require.Empty(t, err, "Unable to create random header block, Error:%v", err)
+//
+//	msgCheckpoint:= checkpoint.NewMsgCheckpointBlock(header.Proposer,header.StartBlock,header.EndBlock,header.RootHash,uint64(time.Now().Unix()))
+//	got := checkpoint.HandleMsgCheckpoint(ctx, msgCheckpoint, keeper)
+//	require.True(t, got.IsOK(), "expected send-checkpoint to be ok, got %v", got)
+//
+//	// check if cache is set
+//	found:=keeper.GetCheckpointCache(ctx,hmcmn.CheckpointCacheKey)
+//	require.Equal(t,true, found,"Checkpoint cache should exist")
+//
+//	// check if checkpoint matches
+//	storedHeader,err:=keeper.GetLastCheckpoint(ctx)
+//	require.Equal(t,header,storedHeader,"Header stored is not same")
+//}
+
 
