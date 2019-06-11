d07d455afb2e7ed8b34a4d6b0fa796e7587d3fba
vaibhavchellani <vaibhavchellani223@gmail.com>
2019-06-11 17:37:02 +0530

    fixed verifier not present in querier issue

--- server/root.go
+++ server/root.go
@@ -9,6 +9,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/tx"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/go-kit/kit/log"
+
 	checkpoint "github.com/maticnetwork/heimdall/checkpoint/rest"
 	staking "github.com/maticnetwork/heimdall/staking/rest"
 	"github.com/spf13/cobra"
@@ -36,6 +37,7 @@ func ServeCommands(cdc *codec.Codec, registerRoutesFn func(*lcd.RestServer)) *co
 	}
 	cmd.Flags().String(client.FlagListenAddr, "tcp://0.0.0.0:1317", "The address for the server to listen on")
 	cmd.Flags().String(client.FlagCORS, "", "Set the domains that can make CORS requests (* for all)")
+	cmd.Flags().Bool(client.FlagTrustNode, true, "Trust connected full node (don't verify proofs for responses)")
 	cmd.Flags().String(client.FlagChainID, "", "The chain ID to connect to")
 	cmd.Flags().String(client.FlagNode, "tcp://localhost:26657", "Address of the node to connect to")
 	cmd.Flags().Int(client.FlagMaxOpenConnections, 1000, "The number of maximum open connections")
