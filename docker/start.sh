25ae622bcac77f56ff53d0351f2eee0792da49f8
venkatesh mankena <mankenavenkatesh@gmail.com>
2019-10-14 14:30:29 +0530

    MAT-138 Update docker image

--- docker/start.sh
+++ docker/start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 # start processes
-./build/heimdalld start > ./logs/heimdalld.log &
-./build/heimdalld rest-server > ./logs/heimdalld-rest-server.log &
+heimdalld start > ./logs/heimdalld.log &
+heimdalld rest-server > ./logs/heimdalld-rest-server.log &
 
 # tail logs
 tail -f ./logs/heimdalld.log ./logs/heimdalld-rest-server.log 
