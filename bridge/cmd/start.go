65234ec68157079c91ae8ff50c389665c8f313f0
venkatesh mankena <mankenavenkatesh@gmail.com>
2019-10-30 15:38:46 +0530

    MAT-286 Review comments

--- bridge/cmd/start.go
+++ bridge/cmd/start.go
@@ -17,88 +17,96 @@ import (
 	"github.com/maticnetwork/heimdall/helper"
 )
 
-// startCmd represents the start command
-var startCmd = &cobra.Command{
-	Use:   "start",
-	Short: "Start bridge server",
-	Run: func(cmd *cobra.Command, args []string) {
-		// create codec
-		cdc := app.MakeCodec()
-		app.MakePulp()
+func startCmd() *cobra.Command {
+	// startCmd represents the start command
+	var startCmd = &cobra.Command{
+		Use:   "start",
+		Short: "Start bridge server",
+		Run: func(cmd *cobra.Command, args []string) {
+			// create codec
+			cdc := app.MakeCodec()
+			app.MakePulp()
 
-		// queue connector & http client
-		_queueConnector := pier.NewQueueConnector(cdc, helper.GetConfig().AmqpURL)
-		_httpClient := httpClient.NewHTTP(helper.GetConfig().TendermintNodeURL, "/websocket")
+			// queue connector & http client
+			_queueConnector := pier.NewQueueConnector(cdc, helper.GetConfig().AmqpURL)
+			_httpClient := httpClient.NewHTTP(helper.GetConfig().TendermintNodeURL, "/websocket")
 
-		// selected services to start
-		services := SelectedServices(cdc, _httpClient, _queueConnector)
-		fmt.Println("Services to start - ", services)
-		if len(services) == 0 {
-			panic(fmt.Sprintf("No services selected to start. select services using --all or --only flag"))
-		}
-
-		// sync group
-		var wg sync.WaitGroup
-
-		// go routine to catch signal
-		catchSignal := make(chan os.Signal, 1)
-		signal.Notify(catchSignal, os.Interrupt)
-		go func() {
-			// sig is a ^C, handle it
-			for range catchSignal {
-				// stop processes
-				for _, service := range services {
-					service.Stop()
-				}
-
-				// stop http client
-				_httpClient.Stop()
-
-				// exit
-				os.Exit(1)
+			// selected services to start
+			services := SelectedServices(cdc, _httpClient, _queueConnector)
+			if len(services) == 0 {
+				panic(fmt.Sprintf("No services selected to start. select services using --all or --only flag"))
 			}
-		}()
 
-		// Start http client
-		err := _httpClient.Start()
-		if err != nil {
-			panic(fmt.Sprintf("Error connecting to server %v", err))
-		}
+			// sync group
+			var wg sync.WaitGroup
 
-		// strt all processes
-		for _, service := range services {
-			go func(serv common.Service) {
-				defer wg.Done()
-				// TODO handle error while starting service
-				serv.Start()
-				<-serv.Quit()
-			}(service)
-		}
-		// wait for all processes
-		wg.Add(len(services))
-		wg.Wait()
-	},
+			// go routine to catch signal
+			catchSignal := make(chan os.Signal, 1)
+			signal.Notify(catchSignal, os.Interrupt)
+			go func() {
+				// sig is a ^C, handle it
+				for range catchSignal {
+					// stop processes
+					for _, service := range services {
+						service.Stop()
+					}
+
+					// stop http client
+					_httpClient.Stop()
+
+					// exit
+					os.Exit(1)
+				}
+			}()
+
+			// Start http client
+			err := _httpClient.Start()
+			if err != nil {
+				panic(fmt.Sprintf("Error connecting to server %v", err))
+			}
+
+			// strt all processes
+			for _, service := range services {
+				go func(serv common.Service) {
+					defer wg.Done()
+					// TODO handle error while starting service
+					serv.Start()
+					<-serv.Quit()
+				}(service)
+			}
+			// wait for all processes
+			wg.Add(len(services))
+			wg.Wait()
+		},
+	}
+
+	startCmd.Flags().Bool("all", false, "start all bridge services")
+	viper.BindPFlag("all", startCmd.Flags().Lookup("all"))
+
+	startCmd.Flags().StringSlice("only", []string{}, "comma separated bridge services to start")
+	viper.BindPFlag("only", startCmd.Flags().Lookup("only"))
+
+	return startCmd
 }
 
 // SelectedServices will select services to start based on set flags --all, --only
 func SelectedServices(cdc *codec.Codec, _httpClient *httpClient.HTTP, _queueConnector *pier.QueueConnector) []common.Service {
 
-	services := []common.Service{}
+	services := []common.Service{
+		pier.NewConsumerService(cdc, _queueConnector),
+	}
 
 	startAll := viper.GetBool("all")
 	onlyServices := viper.GetStringSlice("only")
 
 	if startAll {
-		services = []common.Service{
+		services = append(services,
 			pier.NewCheckpointer(cdc, _queueConnector, _httpClient),
 			pier.NewSyncer(cdc, _queueConnector, _httpClient),
 			pier.NewAckService(cdc, _queueConnector, _httpClient),
 			pier.NewSpanService(cdc, _queueConnector, _httpClient),
 			pier.NewClerkService(cdc, _queueConnector, _httpClient),
-
-			// queue consumer server
-			pier.NewConsumerService(cdc, _queueConnector),
-		}
+		)
 	} else {
 		for _, service := range onlyServices {
 			switch service {
@@ -112,8 +120,6 @@ func SelectedServices(cdc *codec.Codec, _httpClient *httpClient.HTTP, _queueConn
 				services = append(services, pier.NewSpanService(cdc, _queueConnector, _httpClient))
 			case "clerk":
 				services = append(services, pier.NewClerkService(cdc, _queueConnector, _httpClient))
-			case "consumer":
-				services = append(services, pier.NewConsumerService(cdc, _queueConnector))
 			}
 		}
 	}
@@ -122,12 +128,5 @@ func SelectedServices(cdc *codec.Codec, _httpClient *httpClient.HTTP, _queueConn
 }
 
 func init() {
-
-	startCmd.Flags().Bool("all", false, "start all bridge services")
-	viper.BindPFlag("all", startCmd.Flags().Lookup("all"))
-
-	startCmd.Flags().StringSlice("only", []string{}, "comma separated bridge services to start")
-	viper.BindPFlag("only", startCmd.Flags().Lookup("only"))
-
-	rootCmd.AddCommand(startCmd)
+	rootCmd.AddCommand(startCmd())
 }
