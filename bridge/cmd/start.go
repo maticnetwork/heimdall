3124a8f647cc93eea294da178a4417569fa76732
venkatesh mankena <mankenavenkatesh@gmail.com>
2019-10-30 12:34:47 +0530

    MAT-286 - option to start selected services in bridge

--- bridge/cmd/start.go
+++ bridge/cmd/start.go
@@ -6,7 +6,9 @@ import (
 	"os/signal"
 	"sync"
 
+	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"github.com/tendermint/tendermint/libs/common"
 	httpClient "github.com/tendermint/tendermint/rpc/client"
 
@@ -28,15 +30,11 @@ var startCmd = &cobra.Command{
 		_queueConnector := pier.NewQueueConnector(cdc, helper.GetConfig().AmqpURL)
 		_httpClient := httpClient.NewHTTP(helper.GetConfig().TendermintNodeURL, "/websocket")
 
-		services := [...]common.Service{
-			pier.NewCheckpointer(cdc, _queueConnector, _httpClient),
-			pier.NewSyncer(cdc, _queueConnector, _httpClient),
-			pier.NewAckService(cdc, _queueConnector, _httpClient),
-			pier.NewSpanService(cdc, _queueConnector, _httpClient),
-			pier.NewClerkService(cdc, _queueConnector, _httpClient),
-
-			// queue consumer server
-			pier.NewConsumerService(cdc, _queueConnector),
+		// selected services to start
+		services := SelectedServices(cdc, _httpClient, _queueConnector)
+		fmt.Println("Services to start - ", services)
+		if len(services) == 0 {
+			panic(fmt.Sprintf("No services selected to start. select services using --all or --only flag"))
 		}
 
 		// sync group
@@ -82,6 +80,54 @@ var startCmd = &cobra.Command{
 	},
 }
 
+// SelectedServices will select services to start based on set flags --all, --only
+func SelectedServices(cdc *codec.Codec, _httpClient *httpClient.HTTP, _queueConnector *pier.QueueConnector) []common.Service {
+
+	services := []common.Service{}
+
+	startAll := viper.GetBool("all")
+	onlyServices := viper.GetStringSlice("only")
+
+	if startAll {
+		services = []common.Service{
+			pier.NewCheckpointer(cdc, _queueConnector, _httpClient),
+			pier.NewSyncer(cdc, _queueConnector, _httpClient),
+			pier.NewAckService(cdc, _queueConnector, _httpClient),
+			pier.NewSpanService(cdc, _queueConnector, _httpClient),
+			pier.NewClerkService(cdc, _queueConnector, _httpClient),
+
+			// queue consumer server
+			pier.NewConsumerService(cdc, _queueConnector),
+		}
+	} else {
+		for _, service := range onlyServices {
+			switch service {
+			case "checkpoint":
+				services = append(services, pier.NewCheckpointer(cdc, _queueConnector, _httpClient))
+			case "syncer":
+				services = append(services, pier.NewSyncer(cdc, _queueConnector, _httpClient))
+			case "ack":
+				services = append(services, pier.NewAckService(cdc, _queueConnector, _httpClient))
+			case "span":
+				services = append(services, pier.NewSpanService(cdc, _queueConnector, _httpClient))
+			case "clerk":
+				services = append(services, pier.NewClerkService(cdc, _queueConnector, _httpClient))
+			case "consumer":
+				services = append(services, pier.NewConsumerService(cdc, _queueConnector))
+			}
+		}
+	}
+
+	return services
+}
+
 func init() {
+
+	startCmd.Flags().Bool("all", false, "start all bridge services")
+	viper.BindPFlag("all", startCmd.Flags().Lookup("all"))
+
+	startCmd.Flags().StringSlice("only", []string{}, "comma separated bridge services to start")
+	viper.BindPFlag("only", startCmd.Flags().Lookup("only"))
+
 	rootCmd.AddCommand(startCmd)
 }
