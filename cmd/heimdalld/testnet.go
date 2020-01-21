dfa9dc220c47b9f06516f7773adc4d354de01a1c
Vaibhav Chellani <vaibhavchellani223@gmail.com>
2020-01-21 17:52:06 +0530

    fix select default num of producers for first span

--- cmd/heimdalld/testnet.go
+++ cmd/heimdalld/testnet.go
@@ -109,7 +109,6 @@ testnet --v 4 --n 8 --output-dir ./output --starting-ip-address 192.168.10.2
 				}
 
 				genFiles[i] = config.GenesisFile()
-
 				newPubkey := CryptoKeyToPubkey(valPubKeys[i])
 
 				// create validator account
