e2a7ea49f0326d118d471b84aecefb0f30f5ec8e
Sayli <ptsayli@gmail.com>
2020-03-03 17:02:12 +0530

    Remove topup functions from bank module

--- topup/types/codec.go
+++ topup/types/codec.go
@@ -9,11 +9,13 @@ import (
 // RegisterCodec registers concrete types on codec codec
 func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterConcrete(MsgTopup{}, "topup/MsgTopup", nil)
+	cdc.RegisterConcrete(MsgWithdrawFee{}, "bank/MsgWithdrawFee", nil)
 }
 
 // RegisterPulp register pulp
 func RegisterPulp(pulp *authTypes.Pulp) {
 	pulp.RegisterConcrete(MsgTopup{})
+	pulp.RegisterConcrete(MsgWithdrawFee{})
 }
 
 // ModuleCdc module cdc
