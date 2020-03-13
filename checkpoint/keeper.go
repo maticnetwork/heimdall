d52325806a3d430aaa8628d212f61d50e3b322ca
Jaynti Kanani <jdkanani@gmail.com>
2020-03-13 22:26:10 +0530

    add checkpoint buffer time to params

--- checkpoint/keeper.go
+++ checkpoint/keeper.go
@@ -300,3 +300,17 @@ func (k Keeper) UpdateACKCount(ctx sdk.Context) {
 	// update
 	store.Set(ACKCountKey, ACKs)
 }
+
+// -----------------------------------------------------------------------------
+// Params
+
+// SetParams sets the auth module's parameters.
+func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
+	k.paramSpace.SetParamSet(ctx, &params)
+}
+
+// GetParams gets the auth module's parameters.
+func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
+	k.paramSpace.GetParamSet(ctx, &params)
+	return
+}
