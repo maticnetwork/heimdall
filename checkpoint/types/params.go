09a13aa3b16adaf41baaa173394c893758e7488c
Sayli <ptsayli@gmail.com>
2020-04-22 12:48:17 +0530

    new: add checkpoint length to params

--- checkpoint/types/params.go
+++ checkpoint/types/params.go
@@ -12,11 +12,13 @@ import (
 // Default parameter values
 const (
 	DefaultCheckpointBufferTime time.Duration = 1000 * time.Second // Time checkpoint is allowed to stay in buffer (1000 seconds ~ 17 mins)
+	DefaultCheckpointLength     uint          = 256
 )
 
 // Parameter keys
 var (
 	KeyCheckpointBufferTime = []byte("CheckpointBufferTime")
+	KeyCheckpointLength     = []byte("CheckpointLength")
 )
 
 var _ subspace.ParamSet = &Params{}
@@ -24,14 +26,17 @@ var _ subspace.ParamSet = &Params{}
 // Params defines the parameters for the auth module.
 type Params struct {
 	CheckpointBufferTime time.Duration `json:"checkpoint_buffer_time" yaml:"checkpoint_buffer_time"`
+	CheckpointLength     uint          `json:"checkpoint_length" yaml:"checkpoint_length"`
 }
 
 // NewParams creates a new Params object
 func NewParams(
 	checkpointBufferTime time.Duration,
+	checkpointLength uint,
 ) Params {
 	return Params{
 		CheckpointBufferTime: checkpointBufferTime,
+		CheckpointLength:     checkpointLength,
 	}
 }
 
@@ -46,6 +51,7 @@ func ParamKeyTable() subspace.KeyTable {
 func (p *Params) ParamSetPairs() subspace.ParamSetPairs {
 	return subspace.ParamSetPairs{
 		{KeyCheckpointBufferTime, &p.CheckpointBufferTime},
+		{KeyCheckpointLength, &p.CheckpointLength},
 	}
 }
 
@@ -60,6 +66,7 @@ func (p Params) Equal(p2 Params) bool {
 func DefaultParams() Params {
 	return Params{
 		CheckpointBufferTime: DefaultCheckpointBufferTime,
+		CheckpointLength:     DefaultCheckpointLength,
 	}
 }
 
@@ -68,6 +75,7 @@ func (p Params) String() string {
 	var sb strings.Builder
 	sb.WriteString("Params: \n")
 	sb.WriteString(fmt.Sprintf("CheckpointBufferTime: %s\n", p.CheckpointBufferTime))
+	sb.WriteString(fmt.Sprintf("CheckpointLength: %s\n", string(p.CheckpointLength)))
 	return sb.String()
 }
 
