0db16e6576dfd922863e541436f9224680641d81
Arpit Temani <temaniarpit27@gmail.com>
2020-04-29 12:23:13 +0530

    test: add: keeper test cases

---
+++ clerk/integration_test.go
@@ -0,0 +1,19 @@
+package clerk_test
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	abci "github.com/tendermint/tendermint/abci/types"
+
+	"github.com/maticnetwork/heimdall/app"
+)
+
+//
+// Create test app
+//
+
+// returns context and app on clerk keeper
+func createTestApp(isCheckTx bool) (*app.HeimdallApp, sdk.Context) {
+	app := app.Setup(isCheckTx)
+	ctx := app.BaseApp.NewContext(isCheckTx, abci.Header{})
+	return app, ctx
+}
