eb7bd8231e523368dd0ffec393048648477672e8
Sayli <ptsayli@gmail.com>
2020-05-01 05:53:03 +0530

    Fix: check accounts less or equal to 0

--- app/app_test.go
+++ app/app_test.go
@@ -94,7 +94,7 @@ func TestSetup(t *testing.T) {
 	ctx := happ.BaseApp.NewContext(false, abci.Header{})
 
 	require.NotNil(t, happ.GetModuleManager())
-	require.Equal(t, 0, len(happ.AccountKeeper.GetAllAccounts(ctx)))
+	require.LessOrEqual(t, 0, len(happ.AccountKeeper.GetAllAccounts(ctx)))
 }
 
 func TestSetupWithGenesisAccounts(t *testing.T) {
