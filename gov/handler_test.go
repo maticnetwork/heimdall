9f0b5eb7732c8b33e628fec8af29e32b213ab337
Arpit Temani <temaniarpit27@gmail.com>
2020-09-08 23:50:14 +0530

    add: handler test cases

--- gov/handler_test.go
+++ gov/handler_test.go
@@ -155,3 +155,50 @@ func (suite *HandlerTestSuite) TestHandleMsgDeposit() {
 		require.True(t, result.IsOK(), "expected submit proposal to be ok, got %v", result)
 	})
 }
+
+func (suite *HandlerTestSuite) TestHandleMsgVote() {
+	t, app, ctx, _, _ := suite.T(), suite.app, suite.ctx, suite.chainID, suite.r
+
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	n := 5
+
+	validators := make([]*hmTypes.Validator, n)
+	accounts := simulation.RandomAccounts(r1, n)
+
+	for i := range validators {
+		// validator
+		validators[i] = hmTypes.NewValidator(
+			hmTypes.NewValidatorID(uint64(int64(i))),
+			0,
+			0,
+			1,
+			int64(simulation.RandIntBetween(r1, 10, 100)), // power
+			hmTypes.NewPubKey(accounts[i].PubKey.Bytes()),
+			accounts[i].Address,
+		)
+
+		err := app.StakingKeeper.AddValidator(ctx, *validators[i])
+		if err != nil {
+			t.Error("Error while adding validator to store", err)
+		}
+	}
+
+	app.BankKeeper.AddCoins(ctx, accounts[0].Address, sdk.NewCoins(sdk.NewCoin(authTypes.FeeToken, sdk.NewInt(int64(4*10)))))
+	tp := testProposal()
+	proposal, _ := app.GovKeeper.SubmitProposal(ctx, tp)
+	app.GovKeeper.ActivateVotingPeriod(ctx, proposal)
+	proposalID := proposal.ProposalID
+
+	msg := types.NewMsgVote(
+		accounts[0].Address,
+		proposalID,
+		types.OptionYes,
+		validators[0].ID,
+	)
+
+	t.Run("Success", func(t *testing.T) {
+		result := suite.handler(ctx, msg)
+		require.True(t, result.IsOK(), "expected add vote to be ok, got %v", result)
+	})
+}
