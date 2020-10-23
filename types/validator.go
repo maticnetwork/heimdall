5a7ea333690cf0ee004872860208a875e78561af
venkatesh mankena <mankenavenkatesh@gmail.com>
2020-10-23 12:46:23 +0530

    merge with new-heimdall branch

--- types/validator.go
+++ types/validator.go
@@ -1,21 +1,21 @@
 package types
 
-func NewValidator(
-	id ValidatorID,
-	startEpoch uint64,
-	endEpoch uint64,
-	nonce uint64,
-	power int64,
-	pubKey PubKey,
-	signer HeimdallAddress,
-) *Validator {
-	return &Validator{
-		ID:          id,
-		StartEpoch:  startEpoch,
-		EndEpoch:    endEpoch,
-		Nonce:       nonce,
-		VotingPower: power,
-		PubKey:      pubKey,
-		Signer:      signer,
-	}
-}
+// func NewValidator(
+// 	id ValidatorID,
+// 	startEpoch uint64,
+// 	endEpoch uint64,
+// 	nonce uint64,
+// 	power int64,
+// 	pubKey PubKey,
+// 	signer HeimdallAddress,
+// ) *Validator {
+// 	return &Validator{
+// 		ID:          id,
+// 		StartEpoch:  startEpoch,
+// 		EndEpoch:    endEpoch,
+// 		Nonce:       nonce,
+// 		VotingPower: power,
+// 		PubKey:      pubKey,
+// 		Signer:      signer,
+// 	}
+// }
