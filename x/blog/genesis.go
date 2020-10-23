c84bea634b61bca4fd2d9d9924fd0dc1a5821c00
venkatesh mankena <mankenavenkatesh@gmail.com>
2020-10-23 12:40:59 +0530

    validator types

--- x/blog/genesis.go
+++ x/blog/genesis.go
@@ -4,11 +4,14 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/maticnetwork/heimdall/x/blog/keeper"
 	"github.com/maticnetwork/heimdall/x/blog/types"
+	hmTypes "github.com/maticnetwork/heimdall/types"
 )
 
 // InitGenesis initializes the capability module's state from a provided genesis
 // state.
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
+	Validator v := hmThmTypes.Validator
+
 }
 
 // ExportGenesis returns the capability module's exported genesis.
