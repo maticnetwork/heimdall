e518fb086c6a1eb581fe7760482aadfc80b60ec6
Sandeep Sreenath <sandeep.sreenath@gmail.com>
2020-11-03 16:48:45 +0530

    scaffolded chainmanager code using starport

---
+++ x/chainmanager/genesis.go
@@ -0,0 +1,17 @@
+package chainmanager
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/maticnetwork/heimdall/x/chainmanager/keeper"
+	"github.com/maticnetwork/heimdall/x/chainmanager/types"
+)
+
+// InitGenesis initializes the capability module's state from a provided genesis
+// state.
+func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
+}
+
+// ExportGenesis returns the capability module's exported genesis.
+func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
+	return types.DefaultGenesis()
+}
