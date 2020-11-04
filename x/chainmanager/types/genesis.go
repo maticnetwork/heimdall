90cea83b773161270839dad1b07b63c2527e63cb
Sandeep Sreenath <sandeep.sreenath@gmail.com>
2020-11-04 16:00:29 +0530

    ported functions from genesis.go of develop branch

--- x/chainmanager/types/genesis.go
+++ x/chainmanager/types/genesis.go
@@ -1,5 +1,7 @@
 package types
 
+import "encoding/json"
+
 // DefaultIndex is the default capability global index
 const DefaultIndex uint64 = 1
 
@@ -12,4 +14,33 @@ func DefaultGenesis() *GenesisState {
 // failure.
 func (gs GenesisState) Validate() error {
 	return nil
+}
+
+// Ported from develop branch:
+
+// NewGenesisState - Create a new genesis state
+func NewGenesisState(params *Params) GenesisState {
+	return GenesisState{
+		Params: params,
+	}
+}
+
+// DefaultGenesisState - Return a default genesis state
+func DefaultGenesisState() GenesisState {
+	return NewGenesisState(DefaultParams())
+}
+
+// ValidateGenesis performs basic validation of auth genesis data returning an
+// error for any failed validation criteria.
+func ValidateGenesis(data GenesisState) error {
+	return nil
+}
+
+// GetGenesisStateFromAppState returns staking GenesisState given raw application genesis state
+func GetGenesisStateFromAppState(appState map[string]json.RawMessage) GenesisState {
+	var genesisState GenesisState
+	if appState[ModuleName] != nil {
+		ModuleCdc.MustUnmarshalJSON(appState[ModuleName], &genesisState)
+	}
+	return genesisState
 }
