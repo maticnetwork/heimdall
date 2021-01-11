afd40f11783d8829e32624a91df95163ac3c47dc
Sandeep Sreenath <sandeep.sreenath@gmail.com>
2021-01-11 09:16:28 +0530

    topup test cases porting initial commit

---
+++ x/topup/keeper/common_test.go
@@ -0,0 +1,32 @@
+package keeper_test
+
+import (
+	"math/rand"
+	"strconv"
+	"time"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	abci "github.com/tendermint/tendermint/abci/types"
+
+	"github.com/maticnetwork/heimdall/app"
+	"github.com/maticnetwork/heimdall/types/simulation"
+)
+
+//
+// Create test app
+//
+
+// returns context and app with params set on chainmanager keeper
+func createTestApp(isCheckTx bool) (*app.HeimdallApp, sdk.Context, client.Context) {
+	app := app.Setup(isCheckTx)
+	ctx := app.BaseApp.NewContext(isCheckTx, abci.Header{})
+	cliCtx := client.Context{}.WithJSONMarshaler(app.AppCodec())
+
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+
+	topupSequence := strconv.Itoa(simulation.RandIntBetween(r1, 1000, 100000))
+	app.TopupKeeper.SetTopupSequence(ctx, topupSequence)
+	return app, ctx, cliCtx
+}
