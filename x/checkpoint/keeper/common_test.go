2482cdd92cf24831d9b102c0e4af864300fcec4e
Sai Kumar <sai@vitwit.com>
2021-01-19 12:16:25 +0530

    test: added test cases to keeper of checkpoint

---
+++ x/checkpoint/keeper/common_test.go
@@ -0,0 +1,64 @@
+package keeper_test
+
+import (
+	"encoding/json"
+	"github.com/cosmos/cosmos-sdk/client"
+	abci "github.com/tendermint/tendermint/abci/types"
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/maticnetwork/heimdall/app"
+	"github.com/maticnetwork/heimdall/helper"
+	hmTypes "github.com/maticnetwork/heimdall/types"
+	"github.com/maticnetwork/heimdall/x/checkpoint/types"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+)
+
+//
+// Create test app
+//
+
+// createTestApp returns context and app
+func createTestApp(isCheckTx bool) (*app.HeimdallApp, sdk.Context, client.Context) {
+	genesisState := app.NewDefaultGenesisState()
+
+	app := app.Setup(isCheckTx)
+	ctx := app.BaseApp.NewContext(isCheckTx, tmproto.Header{})
+	cliCtx := client.Context{}.WithJSONMarshaler(app.AppCodec())
+
+	helper.SetTestConfig(helper.GetDefaultHeimdallConfig())
+
+	params := types.NewParams(5*time.Second, 256, 1024, 10000)
+
+	Checkpoints := make([]hmTypes.Checkpoint, 0)
+
+	for i := range Checkpoints {
+		Checkpoints[i] = hmTypes.Checkpoint{}
+	}
+
+	checkpointGenesis := types.NewGenesisState(
+		types.DefaultGenesis().Params,
+		types.DefaultGenesis().BufferedCheckpoint,
+		types.DefaultGenesis().LastNoACK,
+		types.DefaultGenesis().AckCount,
+		types.DefaultGenesis().Checkpoints,
+	)
+
+	genesisState[types.ModuleName] = app.AppCodec().MustMarshalJSON(&checkpointGenesis)
+
+	stateBytes, err := json.MarshalIndent(genesisState, "", " ")
+	if err != nil {
+		panic(err)
+	}
+
+	app.InitChain(
+		abci.RequestInitChain{
+			Validators:    []abci.ValidatorUpdate{},
+			AppStateBytes: stateBytes,
+		},
+	)
+	app.Commit()
+	app.BeginBlock(abci.RequestBeginBlock{Header: tmproto.Header{Height: app.LastBlockHeight() + 1}})
+	app.CheckpointKeeper.SetParams(ctx, params)
+	return app, ctx, cliCtx
+}
---
+++ x/checkpoint/keeper/keeper_test.go
@@ -0,0 +1,117 @@
+package keeper_test
+
+import (
+	checkpointKeeper "github.com/maticnetwork/heimdall/x/checkpoint/keeper"
+	"testing"
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/maticnetwork/heimdall/app"
+	hmTypes "github.com/maticnetwork/heimdall/types"
+	hmCommonTypes "github.com/maticnetwork/heimdall/types/common"
+
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type KeeperTestSuite struct {
+	suite.Suite
+
+	app *app.HeimdallApp
+	ctx sdk.Context
+}
+
+func (suite *KeeperTestSuite) SetupTest() {
+	suite.app, suite.ctx, _ = createTestApp(false)
+}
+
+func TestKeeperTestSuite(t *testing.T) {
+	suite.Run(t, new(KeeperTestSuite))
+}
+
+func (suite *KeeperTestSuite) TestAddCheckpoint() {
+	t, initApp, ctx := suite.T(), suite.app, suite.ctx
+	keeper := initApp.CheckpointKeeper
+
+	headerBlockNumber := uint64(2000)
+	startBlock := uint64(0)
+	endBlock := uint64(256)
+	rootHash := hmCommonTypes.HexToHeimdallHash("123")
+	proposerAddress := hmCommonTypes.HexToHeimdallAddress("123")
+	timestamp := uint64(time.Now().Unix())
+	borChainId := "1234"
+
+	Checkpoint := hmTypes.CreateBlock(
+		startBlock,
+		endBlock,
+		rootHash,
+		proposerAddress,
+		borChainId,
+		timestamp,
+	)
+	err := keeper.AddCheckpoint(ctx, headerBlockNumber, &Checkpoint)
+	require.NoError(t, err)
+
+	result, err := keeper.GetCheckpointByNumber(ctx, headerBlockNumber)
+	require.NoError(t, err)
+	require.Equal(t, startBlock, result.StartBlock)
+	require.Equal(t, endBlock, result.EndBlock)
+	require.Equal(t, rootHash, hmCommonTypes.HexToHeimdallHash(hmCommonTypes.BytesToHeimdallAddress(result.RootHash).String()))
+	require.Equal(t, borChainId, result.BorChainID)
+	require.Equal(t, proposerAddress, hmCommonTypes.HexToHeimdallAddress(result.Proposer))
+	require.Equal(t, timestamp, result.TimeStamp)
+}
+
+func (suite *KeeperTestSuite) TestGetCheckpointList() {
+	t, initApp, ctx := suite.T(), suite.app, suite.ctx
+	keeper := initApp.CheckpointKeeper
+
+	count := 5
+
+	startBlock := uint64(0)
+	endBlock := uint64(0)
+
+	for i := 0; i < count; i++ {
+		headerBlockNumber := uint64(i) + 1
+
+		startBlock = startBlock + endBlock
+		endBlock = endBlock + uint64(255)
+		rootHash := hmCommonTypes.HexToHeimdallHash("123")
+		proposerAddress := hmCommonTypes.HexToHeimdallAddress("123")
+		timestamp := uint64(time.Now().Unix()) + uint64(i)
+		borChainId := "1234"
+
+		Checkpoint := hmTypes.CreateBlock(
+			startBlock,
+			endBlock,
+			rootHash,
+			proposerAddress,
+			borChainId,
+			timestamp,
+		)
+
+		keeper.AddCheckpoint(ctx, headerBlockNumber, &Checkpoint)
+		keeper.UpdateACKCount(ctx)
+	}
+
+	result, err := keeper.GetCheckpointList(ctx, uint64(1), uint64(20))
+	require.NoError(t, err)
+	require.LessOrEqual(t, count, len(result))
+}
+
+func (suite *KeeperTestSuite) TestHasStoreValue() {
+	t, initApp, ctx := suite.T(), suite.app, suite.ctx
+	keeper := initApp.CheckpointKeeper
+	key := checkpointKeeper.ACKCountKey
+	result := keeper.HasStoreValue(ctx, key)
+	require.True(t, result)
+}
+
+func (suite *KeeperTestSuite) TestFlushCheckpointBuffer() {
+	t, initApp, ctx := suite.T(), suite.app, suite.ctx
+	keeper := initApp.CheckpointKeeper
+	key := checkpointKeeper.BufferCheckpointKey
+	keeper.FlushCheckpointBuffer(ctx)
+	result := keeper.HasStoreValue(ctx, key)
+	require.False(t, result)
+}
