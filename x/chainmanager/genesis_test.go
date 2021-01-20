5a2c091a0e8f5de7c231b869f7f7bbb07c2982fb
Sai Kumar <sai@vitwit.com>
2021-01-20 12:29:16 +0530

    test: added test cases to genesis of  chainmanager

---
+++ x/chainmanager/genesis_test.go
@@ -0,0 +1,45 @@
+package chainmanager_test
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/maticnetwork/heimdall/app"
+	"github.com/maticnetwork/heimdall/x/chainmanager"
+	"github.com/maticnetwork/heimdall/x/chainmanager/types"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+// GenesisTestSuite integrate test suite context object
+type GenesisTestSuite struct {
+	suite.Suite
+
+	app *app.HeimdallApp
+	ctx sdk.Context
+}
+
+// SetupTest setup necessary things for genesis test
+func (suite *GenesisTestSuite) SetupTest() {
+	suite.app, suite.ctx = createTestApp(true)
+}
+
+// TestGenesisTestSuite
+func TestGenesisTestSuite(t *testing.T) {
+	suite.Run(t, new(GenesisTestSuite))
+}
+
+//TestInitExportGenesis test import and export genesis state
+func (suite *GenesisTestSuite) TestInitExportGenesis() {
+	t, app, ctx := suite.T(), suite.app, suite.ctx
+	params := types.DefaultParams()
+
+	genesisState := types.GenesisState{
+		Params: params,
+	}
+	chainmanager.InitGenesis(ctx, app.ChainKeeper, genesisState)
+
+	actualParams := chainmanager.ExportGenesis(ctx, app.ChainKeeper)
+	require.Equal(t, &genesisState, actualParams)
+
+}
