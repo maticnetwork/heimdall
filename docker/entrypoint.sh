e5365470b23c5e5deaf5072d0bdd5bcb5600aa8f
Victor Castell <victor@victorcastell.com>
2021-11-26 15:39:58 +0100

    Run with docker-compose
    With this PR we move towards the Docker philosophy of running just a single process. Running multiple processes without attending signals was not useful and dangerous for graceful stops.
    
    Refactor a bit the entrypoint so it responds to all binaries, heimdalld and bridge.
    
    Create a canonical docker-compose file to run all 3 components configuring them using env vars.
    
    heimdalcli could be run by overriding the entrypoint.
    
    Remove tini as now the main bin is heimdalld and it correctly handle signals.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,10 +4,9 @@ if [ ! -f $HEIMDALL_DIR/config/heimdall-config.toml ]; then
     heimdalld --home=$HEIMDALL_DIR init
 fi;
 
-# start processes
-heimdalld --home=$HEIMDALL_DIR start &
-heimdalld --home=$HEIMDALL_DIR rest-server &
-sleep 100
-bridge --home=$HEIMDALL_DIR start --all
+if [ "$1" = 'bridge' ]; then
+    shift
+    exec bridge --home=$HEIMDALL_DIR "$@"
+fi
 
-exit $?
+exec heimdalld --home=$HEIMDALL_DIR "$@"
