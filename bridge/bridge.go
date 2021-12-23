cd3806dfc6240c8359fcac3f1988017b407551de
Nemanja0x <93836814+Nemanja0x@users.noreply.github.com>
2021-12-23 10:40:19 +0100

    POS-17:Add Heimdall flags for configuration (#754)

--- bridge/bridge.go
+++ bridge/bridge.go
@@ -13,6 +13,9 @@ func main() {
 	var logger = helper.Logger.With("module", "bridge/cmd/")
 	rootCmd := cmd.BridgeCommands(viper.GetViper(), logger, "bridge-main")
 
+	// add heimdall flags
+	helper.DecorateWithHeimdallFlags(rootCmd, viper.GetViper(), logger, "bridge-main")
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
