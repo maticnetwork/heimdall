e3aa6e0e7f9d26a41bb7955de4b57f5cf363de18
Roman Behma <begmaroman@gmail.com>
2022-06-20 13:45:21 +0100

    Added tests

--- helper/backoff.go
+++ helper/backoff.go
@@ -1,14 +1,20 @@
 package helper
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // ExponentialBackoff performs exponential backoff attempts on a given action
 func ExponentialBackoff(action func() error, max int, initial time.Duration) error {
+	if max < 0 {
+		return fmt.Errorf("max number should be more or equal to zero, but %d given", max)
+	}
+
 	wait := initial
 	var err error
 	for i := 0; i < max; i++ {
-		err = action()
-		if err == nil {
+		if err = action(); err == nil {
 			return nil
 		}
 		time.Sleep(wait)
---
+++ helper/backoff_test.go
@@ -0,0 +1,53 @@
+package helper
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestExponentialBackoff(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		i := 0
+		outcomes := []bool{false, false, true}
+		t0 := time.Now()
+		err := ExponentialBackoff(func() error {
+			outcome := outcomes[i]
+			i++
+			if outcome {
+				return nil
+			}
+			return errors.New("bad")
+		}, 3, 150*time.Millisecond)
+
+		elapsed := time.Since(t0)
+
+		require.NoError(t, err)
+		require.Equal(t, i, 3)
+		require.True(t, elapsed >= 450*time.Millisecond)
+	})
+
+	t.Run("failed", func(t *testing.T) {
+		i := 0
+		t0 := time.Now()
+		err := ExponentialBackoff(func() error {
+			i++
+			return errors.New("bad")
+		}, 3, 100*time.Millisecond)
+
+		elapsed := time.Since(t0)
+
+		require.Error(t, err)
+		require.Equal(t, i, 3)
+		require.True(t, elapsed >= 600*time.Millisecond)
+	})
+
+	t.Run("failed with negative max value", func(t *testing.T) {
+		err := ExponentialBackoff(func() error {
+			return nil
+		}, -1, 100*time.Millisecond)
+		require.Equal(t, "max number should be more or equal to zero, but -1 given", err.Error())
+	})
+}
