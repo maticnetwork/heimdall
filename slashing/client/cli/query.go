3fd5ad5e7974731429f2791eab3f33c7c445a48f
Vaibhav Jindal <vaibhavjindal29@gmail.com>
2022-07-19 21:59:38 +0530

    Added CLI to slashing module

--- slashing/client/cli/query.go
+++ slashing/client/cli/query.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"encoding/json"
 	"fmt"
 	"strings"
 
@@ -8,8 +9,10 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 
 	"github.com/maticnetwork/heimdall/slashing/types"
+	hmTypes "github.com/maticnetwork/heimdall/types"
 )
 
 // GetQueryCmd returns the cli query commands for this module
@@ -74,6 +77,248 @@ $ <appcli> query slashing signing-info {valID}
 	}
 } */
 
+//Give signing info by id
+func GetSigningInfo(cdc *codec.Codec) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "signing-info",
+		Short: "show signing-info by id",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+
+			id := viper.GetUint64(FlagId)
+
+			params := types.NewQuerySigningInfoParams(hmTypes.ValidatorID(id))
+
+			bz, err := cliCtx.Codec.MarshalJSON(params)
+			if err != nil {
+				return err
+			}
+
+			route := fmt.Sprintf("custom/%s/%s", types.QuerierRoute, types.QuerySigningInfo)
+			res, _, err := cliCtx.QueryWithData(route, bz)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(string(res))
+			return nil
+		},
+	}
+
+	cmd.Flags().String(FlagId, "", "--id=<id here>")
+
+	if err := cmd.MarkFlagRequired(FlagValidatorAddress); err != nil {
+		logger.Error("GetSigningInfo | MarkFlagRequired | FlagId", "Error", err)
+	}
+
+	return cmd
+}
+
+//Give signing info by id
+func GetSigningInfos(cdc *codec.Codec) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "signing-infos",
+		Short: "show signing-info by id",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+
+			page := viper.GetInt(FlagPage)
+
+			limit := viper.GetInt(FlagLimit)
+
+			params := types.NewQuerySigningInfosParams(page, limit)
+
+			bz, err := cliCtx.Codec.MarshalJSON(params)
+			if err != nil {
+				return err
+			}
+
+			route := fmt.Sprintf("custom/%s/%s", types.QuerierRoute, types.QuerySigningInfos)
+			res, _, err := cliCtx.QueryWithData(route, bz)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(string(res))
+			return nil
+		},
+	}
+
+	cmd.Flags().Uint64(FlagPage, 0, "--page=<page number here>")
+	cmd.Flags().Uint64(FlagLimit, 0, "--id=<limit here>")
+
+	if err := cmd.MarkFlagRequired(FlagPage); err != nil {
+		logger.Error("GetSigningInfos | MarkFlagRequired | FlagPage", "Error", err)
+	}
+
+	if err := cmd.MarkFlagRequired(FlagLimit); err != nil {
+		logger.Error("GetSigningInfos | MarkFlagRequired | FlagLimit", "Error", err)
+	}
+
+	return cmd
+}
+
+func GetLatestSlashInfo(cdc *codec.Codec) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "slashing-info",
+		Short: "show latest slash info by id",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+
+			id := viper.GetUint64(FlagId)
+
+			params := types.NewQuerySlashingInfoParams(hmTypes.ValidatorID(id))
+
+			bz, err := cliCtx.Codec.MarshalJSON(params)
+			if err != nil {
+				return err
+			}
+
+			route := fmt.Sprintf("custom/%s/%s", types.QuerierRoute, types.QuerySlashingInfo)
+			res, _, err := cliCtx.QueryWithData(route, bz)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(string(res))
+			return nil
+		},
+	}
+
+	cmd.Flags().String(FlagId, "", "--id=<id here>")
+
+	if err := cmd.MarkFlagRequired(FlagValidatorAddress); err != nil {
+		logger.Error("GetSigningInfo | MarkFlagRequired | FlagId", "Error", err)
+	}
+
+	return cmd
+}
+
+//Give signing info by id
+func GetLatestSlashingInfos(cdc *codec.Codec) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "slashing-infos",
+		Short: "show slashing infos",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+
+			page := viper.GetInt(FlagPage)
+
+			limit := viper.GetInt(FlagLimit)
+
+			params := types.NewQuerySlashingInfosParams(page, limit)
+
+			bz, err := cliCtx.Codec.MarshalJSON(params)
+			if err != nil {
+				return err
+			}
+
+			route := fmt.Sprintf("custom/%s/%s", types.QuerierRoute, types.QuerySlashingInfos)
+			res, _, err := cliCtx.QueryWithData(route, bz)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(string(res))
+			return nil
+		},
+	}
+
+	cmd.Flags().Uint64(FlagPage, 0, "--page=<page number here>")
+	cmd.Flags().Uint64(FlagLimit, 0, "--id=<limit here>")
+
+	if err := cmd.MarkFlagRequired(FlagPage); err != nil {
+		logger.Error("GetCheckpointList | MarkFlagRequired | FlagPage", "Error", err)
+	}
+
+	if err := cmd.MarkFlagRequired(FlagLimit); err != nil {
+		logger.Error("GetCheckpointList | MarkFlagRequired | FlagLimit", "Error", err)
+	}
+
+	return cmd
+}
+
+//Give tick slash infos
+func GetTickSlashingInfos(cdc *codec.Codec) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "tick-slash-infos",
+		Short: "show tick slash infos",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+
+			page := viper.GetInt(FlagPage)
+
+			limit := viper.GetInt(FlagLimit)
+
+			params := types.NewQueryTickSlashingInfosParams(page, limit)
+
+			bz, err := cliCtx.Codec.MarshalJSON(params)
+			if err != nil {
+				return err
+			}
+
+			route := fmt.Sprintf("custom/%s/%s", types.QuerierRoute, types.QueryTickSlashingInfos)
+			res, _, err := cliCtx.QueryWithData(route, bz)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(string(res))
+			return nil
+		},
+	}
+
+	cmd.Flags().Uint64(FlagPage, 0, "--page=<page number here>")
+	cmd.Flags().Uint64(FlagLimit, 0, "--id=<limit here>")
+
+	if err := cmd.MarkFlagRequired(FlagPage); err != nil {
+		logger.Error("GetCheckpointList | MarkFlagRequired | FlagPage", "Error", err)
+	}
+
+	if err := cmd.MarkFlagRequired(FlagLimit); err != nil {
+		logger.Error("GetCheckpointList | MarkFlagRequired | FlagLimit", "Error", err)
+	}
+
+	return cmd
+}
+
+func GetLatestSlashInfoBytes(cdc *codec.Codec) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "latest-slash-info-bytes",
+		Short: "Give the latest slash info bytes",
+		Args:  cobra.NoArgs,
+
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+
+			res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/%s", types.QuerierRoute, types.QuerySlashingInfoBytes), nil)
+
+			if err != nil {
+				return err
+			}
+
+			// error if no slashInfoBytes found
+			if len(res) == 0 {
+				fmt.Printf("no slashing Bytes found")
+				return nil
+			}
+
+			var slashInfoBytes = hmTypes.BytesToHexBytes(res)
+
+			result, err := json.Marshal(&slashInfoBytes)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(string(result))
+			return nil
+
+		},
+	}
+
+	return cmd
+}
+
 // GetCmdQueryParams implements a command to fetch slashing parameters.
 func GetCmdQueryParams(cdc *codec.Codec) *cobra.Command {
 	return &cobra.Command{
@@ -98,4 +343,99 @@ $ <appcli> query slashing params
 			return cliCtx.PrintOutput(params)
 		},
 	}
+}
+
+// Check whether the transaction is old
+func IsOldTx(cdc *codec.Codec) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "is-old-tx",
+		Short: "Check whether the transaction is old",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+
+			// tx hash
+			txHash := viper.GetString(FlagTxHash)
+			if txHash == "" {
+				return fmt.Errorf("tx hash cannot be empty")
+			}
+
+			// log index
+			logIndex := viper.GetUint64(FlagLogIndex)
+
+			// get query params
+			queryParams, err := cliCtx.Codec.MarshalJSON(types.NewQuerySlashingSequenceParams(txHash, logIndex))
+			if err != nil {
+				return err
+			}
+
+			seqNo, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/%s", types.QuerierRoute, types.QuerySlashingSequence), queryParams)
+			if err != nil {
+				return err
+			}
+
+			// error if no tx status found
+			if len(seqNo) == 0 {
+				fmt.Printf("false")
+				return nil
+			}
+
+			res := true
+
+			fmt.Println(res)
+			return nil
+		},
+	}
+
+	cmd.Flags().Uint64(FlagLogIndex, 0, "--log-index=<log index here>")
+	cmd.Flags().Uint64(FlagTxHash, 0, "--tx-hash=<tx hash here>")
+
+	if err := cmd.MarkFlagRequired(FlagLogIndex); err != nil {
+		logger.Error("IsOldTx | MarkFlagRequired | FlagLogIndex", "Error", err)
+	}
+
+	if err := cmd.MarkFlagRequired(FlagTxHash); err != nil {
+		logger.Error("IsOldTx | MarkFlagRequired | FlagTxHash", "Error", err)
+	}
+
+	return cmd
+}
+
+func GetTickCount(cdc *codec.Codec) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "tick-count",
+		Short: "Give the tick-count",
+		Args:  cobra.NoArgs,
+
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+
+			res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/%s", types.QuerierRoute, types.QueryTickCount), nil)
+
+			if err != nil {
+				return err
+			}
+
+			// error if no slashInfoBytes found
+			if len(res) == 0 {
+				fmt.Printf("no slashing Bytes found")
+				return nil
+			}
+
+			var tickCount uint64
+			if err := json.Unmarshal(res, &tickCount); err != nil {
+				return err
+			}
+
+			result, err := json.Marshal(&tickCount)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(string(result))
+			return nil
+
+		},
+	}
+
+	return cmd
 }
