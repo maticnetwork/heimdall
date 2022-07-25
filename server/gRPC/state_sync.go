84b1b5d77aeed2f3b7902940d0db11d40ceea39b
Krishna Upadhyaya <krishnau1604@gmail.com>
2022-07-26 01:14:49 +0530

    Fix gRPC event parsing

--- server/gRPC/state_sync.go
+++ server/gRPC/state_sync.go
@@ -4,11 +4,23 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/url"
+	"time"
 
 	cliContext "github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/maticnetwork/heimdall/helper"
 	proto "github.com/maticnetwork/polyproto/heimdall"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
+
+type Event struct {
+	Id         uint64 `json:"id"`
+	Contract   string `json:"contract"`
+	Data       string `json:"data"`
+	TxHash     string `json:"tx_hash"`
+	LogIndex   uint64 `json:"log_index"`
+	BorChainId string `json:"bor_chain_id"`
+	RecordTime string `json:"record_time"`
+}
 
 func (h *HeimdallGRPCServer) StateSyncEvents(req *proto.StateSyncEventsRequest, reply proto.Heimdall_StateSyncEventsServer) error {
 	cliCtx := cliContext.NewCLIContext().WithCodec(h.cdc)
@@ -27,14 +39,7 @@ func (h *HeimdallGRPCServer) StateSyncEvents(req *proto.StateSyncEventsRequest, 
 			return err
 		}
 
-		var eventRecords []*proto.EventRecord
-
-		err = json.Unmarshal(result.Result, &eventRecords)
-		if err != nil {
-			logger.Error("Error unmarshalling event record", "error", err)
-			return nil
-		}
-
+		eventRecords := parseEvents(result.Result)
 		if len(eventRecords) == 0 {
 			break
 		}
@@ -52,6 +57,38 @@ func (h *HeimdallGRPCServer) StateSyncEvents(req *proto.StateSyncEventsRequest, 
 	}
 
 	return nil
+}
+
+func parseEvents(result json.RawMessage) []*proto.EventRecord {
+	var events []Event
+
+	err := json.Unmarshal(result, &events)
+	if err != nil {
+		logger.Error("Error unmarshalling event record", "error", err)
+		return nil
+	}
+
+	eventRecords := make([]*proto.EventRecord, len(events))
+
+	for i, event := range events {
+		eventTime, err := time.Parse(time.RFC3339, event.RecordTime)
+		if err != nil {
+			logger.Error("Error parsing time", "error", err)
+			return nil
+		}
+
+		eventRecords[i] = &proto.EventRecord{
+			ID:       event.Id,
+			Contract: event.Contract,
+			Data:     event.Data,
+			TxHash:   event.TxHash,
+			LogIndex: event.LogIndex,
+			ChainID:  event.BorChainId,
+			Time:     timestamppb.New(eventTime),
+		}
+	}
+
+	return eventRecords
 }
 
 func addParamsToEndpoint(endpoint string, params map[string]string) string {
