1fd4828264e5cc77840ac279875a93bb6149621f
Vaibhav Jindal <vaibhavjindal29@gmail.com>
2022-10-21 15:43:22 +0530

    Rest logs

--- milestone/querier.go
+++ milestone/querier.go
@@ -101,6 +101,8 @@ func handleQueryCount(ctx sdk.Context, req abci.RequestQuery, keeper Keeper) ([]
 
 func handleQueryLatestNoAckMilestone(ctx sdk.Context, req abci.RequestQuery, keeper Keeper) ([]byte, sdk.Error) {
 	res := keeper.GetLastNoAckMilestone(ctx)
+	logger := keeper.Logger(ctx)
+	logger.Error("In Querier", "res", res)
 	res = "testing"
 	bz, err := json.Marshal(res)
 	if err != nil {
