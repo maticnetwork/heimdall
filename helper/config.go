871ba1fce81c3978224a9f8d5ec6a04721af889c
Jerry <jerrycgh@gmail.com>
2022-12-20 18:02:34 -0800

    Add a flag to turn on/off self healing
    * Added a flag to enable and disable selfheal.
    * Disabled selfheal by default if no flag is present in toml config or CLI.
    * Tuned self heal parameters SHStateSyncedInterval and SHStakeUpdateInterval.
    * Add proper logging to stake update selfheal.

--- helper/config.go
+++ helper/config.go
@@ -94,8 +94,9 @@ const (
 	DefaultNoACKPollInterval        = 1010 * time.Second
 	DefaultClerkPollInterval        = 10 * time.Second
 	DefaultSpanPollInterval         = 1 * time.Minute
-	DefaultSHStateSyncedInterval    = 1 * time.Minute
-	DefaultSHStakeUpdateInterval    = 5 * time.Minute
+	DefaultEnableSH                 = false
+	DefaultSHStateSyncedInterval    = 15 * time.Minute
+	DefaultSHStakeUpdateInterval    = 3 * time.Hour
 	DefaultSHMaxDepthDuration       = time.Hour
 
 	DefaultMainchainGasLimit = uint64(5000000)
@@ -160,6 +161,7 @@ type Configuration struct {
 	NoACKPollInterval        time.Duration `mapstructure:"noack_poll_interval"`      // Poll interval for ack service to send no-ack in case of no checkpoints
 	ClerkPollInterval        time.Duration `mapstructure:"clerk_poll_interval"`
 	SpanPollInterval         time.Duration `mapstructure:"span_poll_interval"`
+	EnableSH                 bool          `mapstructure:"enable_self_heal"`         // Enable self healing
 	SHStateSyncedInterval    time.Duration `mapstructure:"sh_state_synced_interval"` // Interval to self-heal StateSynced events if missing
 	SHStakeUpdateInterval    time.Duration `mapstructure:"sh_stake_update_interval"` // Interval to self-heal StakeUpdate events if missing
 	SHMaxDepthDuration       time.Duration `mapstructure:"sh_max_depth_duration"`    // Max duration that allows to suggest self-healing is not needed
@@ -379,6 +381,7 @@ func GetDefaultHeimdallConfig() Configuration {
 		NoACKPollInterval:        DefaultNoACKPollInterval,
 		ClerkPollInterval:        DefaultClerkPollInterval,
 		SpanPollInterval:         DefaultSpanPollInterval,
+		EnableSH:                 DefaultEnableSH,
 		SHStateSyncedInterval:    DefaultSHStateSyncedInterval,
 		SHStakeUpdateInterval:    DefaultSHStakeUpdateInterval,
 		SHMaxDepthDuration:       DefaultSHMaxDepthDuration,
