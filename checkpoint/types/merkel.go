51963722126961ac4ec40f7bddf49ff559d7a540
Yusufsuryanata1999 <107844742+Yusufsuryanata1999@users.noreply.github.com>
2023-02-05 23:59:25 +0800

    Update merkel.go

--- checkpoint/types/merkel.go
+++ checkpoint/types/merkel.go
@@ -1,134 +1,163 @@
-package types
+package helper
 
 import (
-	"bytes"
 	"errors"
+	"fmt"
+	"math/big"
+	"reflect"
+	"strings"
 
-	"github.com/cbergoon/merkletree"
-	"github.com/ethereum/go-ethereum/common"
-
-	"github.com/maticnetwork/heimdall/helper"
-	hmTypes "github.com/maticnetwork/heimdall/types"
-	"github.com/tendermint/crypto/sha3"
+	"github.com/maticnetwork/bor/accounts/abi"
+	"github.com/maticnetwork/bor/common"
+	"github.com/maticnetwork/bor/core/types"
 )
 
-// ValidateCheckpoint - Validates if checkpoint rootHash matches or not
-func ValidateCheckpoint(start uint64, end uint64, rootHash hmTypes.HeimdallHash, checkpointLength uint64, contractCaller helper.IContractCaller, confirmations uint64) (bool, error) {
-	// Check if blocks exist locally
-	if !contractCaller.CheckIfBlocksExist(end + confirmations) {
-		return false, errors.New("blocks not found locally")
-	}
+// Big batch of reflect types for topic reconstruction.
+var (
+	reflectHash    = reflect.TypeOf(common.Hash{})
+	reflectAddress = reflect.TypeOf(common.Address{})
+	reflectBigInt  = reflect.TypeOf(new(big.Int))
+)
 
-	// Compare RootHash
-	root, err := contractCaller.GetRootHash(start, end, checkpointLength)
-	if err != nil {
-		return false, err
-	}
-
-	if bytes.Equal(root, rootHash.Bytes()) {
-		return true, nil
-	}
-
-	return false, nil
-}
-
-// GetAccountRootHash returns roothash of Validator Account State Tree
-func GetAccountRootHash(dividendAccounts []hmTypes.DividendAccount) ([]byte, error) {
-	tree, err := GetAccountTree(dividendAccounts)
-	if err != nil {
-		return nil, err
-	}
-
-	return tree.Root.Hash, nil
-}
-
-// GetAccountTree returns roothash of Validator Account State Tree
-func GetAccountTree(dividendAccounts []hmTypes.DividendAccount) (*merkletree.MerkleTree, error) {
-	// Sort the dividendAccounts by ID
-	dividendAccounts = hmTypes.SortDividendAccountByAddress(dividendAccounts)
-	list := make([]merkletree.Content, len(dividendAccounts))
-
-	for i := 0; i < len(dividendAccounts); i++ {
-		list[i] = dividendAccounts[i]
-	}
-
-	tree, err := merkletree.NewTreeWithHashStrategy(list, sha3.NewLegacyKeccak256)
-	if err != nil {
-		return nil, err
-	}
-
-	return tree, nil
-}
-
-// GetAccountProof returns proof of dividend Account
-func GetAccountProof(dividendAccounts []hmTypes.DividendAccount, userAddr hmTypes.HeimdallAddress) ([]byte, uint64, error) {
-	// Sort the dividendAccounts by user address
-	dividendAccounts = hmTypes.SortDividendAccountByAddress(dividendAccounts)
-
-	var (
-		list    = make([]merkletree.Content, len(dividendAccounts))
-		account hmTypes.DividendAccount
-	)
-
-	index := uint64(0)
-
-	for i := 0; i < len(dividendAccounts); i++ {
-		list[i] = dividendAccounts[i]
-
-		if dividendAccounts[i].User.Equals(userAddr) {
-			account = dividendAccounts[i]
-			index = uint64(i)
+// UnpackLog unpacks log
+func UnpackLog(abiObject *abi.ABI, out interface{}, event string, log *types.Log) error {
+	if len(log.Data) > 0 {
+		if err := abiObject.Unpack(out, event, log.Data); err != nil {
+			return err
 		}
 	}
 
-	tree, err := merkletree.NewTreeWithHashStrategy(list, sha3.NewLegacyKeccak256)
-	if err != nil {
-		return nil, 0, err
+	selectedEvent := EventByID(abiObject, log.Topics[0].Bytes())
+	if selectedEvent == nil {
+		return errors.New("event signature does not match")
 	}
 
-	branchArray, _, err := tree.GetMerklePath(account)
-
-	// concatenate branch array
-	proof := appendBytes32(branchArray...)
-
-	return proof, index, err
-}
-
-// VerifyAccountProof returns proof of dividend Account
-func VerifyAccountProof(dividendAccounts []hmTypes.DividendAccount, userAddr hmTypes.HeimdallAddress, proofToVerify string) (bool, error) {
-	proof, _, err := GetAccountProof(dividendAccounts, userAddr)
-	if err != nil {
-		return false, nil
-	}
-
-	// check proof bytes
-	if bytes.Equal(common.FromHex(proofToVerify), proof) {
-		return true, nil
-	}
-
-	return false, nil
-}
-
-func convertTo32(input []byte) (output [32]byte, err error) {
-	l := len(input)
-	if l > 32 || l == 0 {
-		return
-	}
-
-	copy(output[32-l:], input[:])
-
-	return
-}
-
-func appendBytes32(data ...[]byte) []byte {
-	var result []byte
-
-	for _, v := range data {
-		paddedV, err := convertTo32(v)
-		if err == nil {
-			result = append(result, paddedV[:]...)
+	var indexed abi.Arguments
+	for _, arg := range abiObject.Events[event].Inputs {
+		if arg.Indexed {
+			indexed = append(indexed, arg)
 		}
 	}
+	return parseTopics(out, indexed, log.Topics[1:])
+}
 
+// parseTopics converts the indexed topic fields into actual log field values.
+//
+// Note, dynamic types cannot be reconstructed since they get mapped to Keccak256
+// hashes as the topic value!
+func parseTopics(out interface{}, fields abi.Arguments, topics []common.Hash) error {
+	// Sanity check that the fields and topics match up
+	if len(fields) != len(topics) {
+		return errors.New("topic/field count mismatch")
+	}
+
+	// Iterate over all the fields and reconstruct them from topics
+	for _, arg := range fields {
+		if !arg.Indexed {
+			return errors.New("non-indexed field in topic reconstruction")
+		}
+		field := reflect.ValueOf(out).Elem().FieldByName(capitalise(arg.Name))
+
+		// Try to parse the topic back into the fields based on primitive types
+		switch field.Kind() {
+		case reflect.Bool:
+			if topics[0][common.HashLength-1] == 1 {
+				field.Set(reflect.ValueOf(true))
+			}
+		case reflect.Int8:
+			num := new(big.Int).SetBytes(topics[0][:])
+			field.Set(reflect.ValueOf(int8(num.Int64())))
+
+		case reflect.Int16:
+			num := new(big.Int).SetBytes(topics[0][:])
+			field.Set(reflect.ValueOf(int16(num.Int64())))
+
+		case reflect.Int32:
+			num := new(big.Int).SetBytes(topics[0][:])
+			field.Set(reflect.ValueOf(int32(num.Int64())))
+
+		case reflect.Int64:
+			num := new(big.Int).SetBytes(topics[0][:])
+			field.Set(reflect.ValueOf(num.Int64()))
+
+		case reflect.Uint8:
+			num := new(big.Int).SetBytes(topics[0][:])
+			field.Set(reflect.ValueOf(uint8(num.Uint64())))
+
+		case reflect.Uint16:
+			num := new(big.Int).SetBytes(topics[0][:])
+			field.Set(reflect.ValueOf(uint16(num.Uint64())))
+
+		case reflect.Uint32:
+			num := new(big.Int).SetBytes(topics[0][:])
+			field.Set(reflect.ValueOf(uint32(num.Uint64())))
+
+		case reflect.Uint64:
+			num := new(big.Int).SetBytes(topics[0][:])
+			field.Set(reflect.ValueOf(num.Uint64()))
+
+		default:
+			// Ran out of plain primitive types, try custom types
+			switch field.Type() {
+			case reflectHash: // Also covers all dynamic types
+				field.Set(reflect.ValueOf(topics[0]))
+
+			case reflectAddress:
+				var addr common.Address
+				copy(addr[:], topics[0][common.HashLength-common.AddressLength:])
+				field.Set(reflect.ValueOf(addr))
+
+			case reflectBigInt:
+				num := new(big.Int).SetBytes(topics[0][:])
+				field.Set(reflect.ValueOf(num))
+
+			default:
+				// Ran out of custom types, try the crazies
+				switch {
+				case arg.Type.T == abi.FixedBytesTy:
+					reflect.Copy(field, reflect.ValueOf(topics[0][common.HashLength-arg.Type.Size:]))
+
+				default:
+					return fmt.Errorf("unsupported indexed type: %v", arg.Type)
+				}
+			}
+		}
+		topics = topics[1:]
+	}
+	return nil
+}
+
+// capitalise makes a camel-case string which starts with an upper case character.
+func capitalise(input string) string {
+	for len(input) > 0 && input[0] == '_' {
+		input = input[1:]
+	}
+	if len(input) == 0 {
+		return ""
+	}
+	return toCamelCase(strings.ToUpper(input[:1]) + input[1:])
+}
+
+// toCamelCase converts an under-score string to a camel-case string
+func toCamelCase(input string) string {
+	toupper := false
+
+	result := ""
+	for k, v := range input {
+		switch {
+		case k == 0:
+			result = strings.ToUpper(string(input[0]))
+
+		case toupper:
+			result += strings.ToUpper(string(v))
+			toupper = false
+
+		case v == '_':
+			toupper = true
+
+		default:
+			result += string(v)
+		}
+	}
 	return result
 }
