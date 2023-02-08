30fa1f5f1160ac0e8909af23d2e88ed35a27bb4e
marcello33 <marcelloardizzone@hotmail.it>
2023-02-08 13:46:18 +0100

    dev: add: POS-1180 use const single block for smart contracts events

--- bridge/setu/listener/rootchain_events.go
+++ bridge/setu/listener/rootchain_events.go
@@ -19,7 +19,9 @@ import (
 const (
 	blocksRange   = 1000
 	maxIterations = 1 // 1 iteration (1000 blocks) on rootchain is roughly 3.3 hours, which is enough for backtracking
+)
 
+const (
 	// smart contracts' events names
 	stateSyncedEvent = "StateSynced"
 	stakeUpdateEvent = "StakeUpdate"
