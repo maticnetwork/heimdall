2d164d5b4c05a01fc008af24c3f92a9643b387ed
Vaibhav Jindal <vaibhavjindal29@gmail.com>
2023-06-21 15:52:12 +0530

    Testing change

--- clerk/side_handler.go
+++ clerk/side_handler.go
@@ -89,6 +89,8 @@ func SideHandleMsgEventRecord(ctx sdk.Context, k Keeper, msg types.MsgEventRecor
 		return hmCommon.ErrorSideTx(k.Codespace(), common.CodeInvalidMsg)
 	}
 
+	return hmCommon.ErrorSideTx(k.Codespace(), common.CodeInvalidMsg)
+
 	if !bytes.Equal(eventLog.Data, msg.Data) {
 		if ctx.BlockHeight() > helper.GetSpanOverrideHeight() {
 			if !(len(eventLog.Data) > helper.MaxStateSyncSize && bytes.Equal(msg.Data, hmTypes.HexToHexBytes(""))) {
