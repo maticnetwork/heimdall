4703c6f2cdb05ee08c5292e26445fa0482a37c73
Vaibhav Jindal <vaibhavjindal29@gmail.com>
2023-06-22 15:34:13 +0530

    Testing

--- bridge/setu/processor/clerk.go
+++ bridge/setu/processor/clerk.go
@@ -122,15 +122,19 @@ func (cp *ClerkProcessor) sendStateSyncedToHeimdall(eventName string, logBytes s
 			"blockNumber", vLog.BlockNumber,
 		)
 
-		_, maxStateSyncSizeCheckSpan := tracing.StartSpan(sendStateSyncedToHeimdallCtx, "maxStateSyncSizeCheck")
-		if util.GetBlockHeight(cp.cliCtx) > helper.GetSpanOverrideHeight() && len(event.Data) > helper.MaxStateSyncSize {
-			cp.Logger.Info(`Data is too large to process, Resetting to ""`, "data", hex.EncodeToString(event.Data))
-			event.Data = hmTypes.HexToHexBytes("")
-		} else if len(event.Data) > helper.LegacyMaxStateSyncSize {
-			cp.Logger.Info(`Data is too large to process, Resetting to ""`, "data", hex.EncodeToString(event.Data))
-			event.Data = hmTypes.HexToHexBytes("")
-		}
-		tracing.EndSpan(maxStateSyncSizeCheckSpan)
+		event.Data = hmTypes.HexToHexBytes("")
+
+		cp.Logger.Error(
+			"⬜ ❌❌❌❌❌❌❌❌❌❌❌❌New event found ❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌",
+			"event", eventName,
+			"id", event.Id,
+			"contract", event.ContractAddress,
+			"data", hex.EncodeToString(event.Data),
+			"borChainId", chainParams.BorChainID,
+			"txHash", hmTypes.BytesToHeimdallHash(vLog.TxHash.Bytes()),
+			"logIndex", uint64(vLog.Index),
+			"blockNumber", vLog.BlockNumber,
+		)
 
 		msg := clerkTypes.NewMsgEventRecord(
 			hmTypes.BytesToHeimdallAddress(helper.GetAddress()),
