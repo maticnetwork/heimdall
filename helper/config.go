693230387e28e6a7a1c39b20e5d362bdb58a5115
Shivam Sharma <shivam691999@gmail.com>
2024-01-03 18:24:08 +0530

    add : amoy seeds

--- helper/config.go
+++ helper/config.go
@@ -122,7 +122,9 @@ const (
 
 	DefaultMainnetSeeds = "1500161dd491b67fb1ac81868952be49e2509c9f@52.78.36.216:26656,dd4a3f1750af5765266231b9d8ac764599921736@3.36.224.80:26656,8ea4f592ad6cc38d7532aff418d1fb97052463af@34.240.245.39:26656,e772e1fb8c3492a9570a377a5eafdb1dc53cd778@54.194.245.5:26656"
 
-	DefaultTestnetSeeds = "9df7ae4bf9b996c0e3436ed4cd3050dbc5742a28@43.200.206.40:26656,d9275750bc877b0276c374307f0fd7eae1d71e35@54.216.248.9:26656,1a3258eb2b69b235d4749cf9266a94567d6c0199@52.214.83.78:26656"
+	DefaultMumbaiTestnetSeeds = "9df7ae4bf9b996c0e3436ed4cd3050dbc5742a28@43.200.206.40:26656,d9275750bc877b0276c374307f0fd7eae1d71e35@54.216.248.9:26656,1a3258eb2b69b235d4749cf9266a94567d6c0199@52.214.83.78:26656"
+
+	DefaultAmoyTestnetSeeds = "eb57fffe96d74312963ced94a94cbaf8e0d8ec2e@54.217.171.196:26656,080dcdffcc453367684b61d8f3ce032f357b0f73@13.251.184.185:26656"
 
 	secretFilePerm = 0600
 
@@ -954,7 +956,6 @@ func DecorateWithTendermintFlags(cmd *cobra.Command, v *viper.Viper, loggerInsta
 	}
 }
 
-// TODO : 0xSharma : add amoy Seeds
 // UpdateTendermintConfig updates tenedermint config with flags and default values if needed
 func UpdateTendermintConfig(tendermintConfig *cfg.Config, v *viper.Viper) {
 	// update tendermintConfig.P2P.Seeds
@@ -968,7 +969,9 @@ func UpdateTendermintConfig(tendermintConfig *cfg.Config, v *viper.Viper) {
 		case MainChain:
 			tendermintConfig.P2P.Seeds = DefaultMainnetSeeds
 		case MumbaiChain:
-			tendermintConfig.P2P.Seeds = DefaultTestnetSeeds
+			tendermintConfig.P2P.Seeds = DefaultMumbaiTestnetSeeds
+		case AmoyChain:
+			tendermintConfig.P2P.Seeds = DefaultAmoyTestnetSeeds
 		}
 	}
 }
