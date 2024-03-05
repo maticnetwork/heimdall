eac0a4617f5590bf3a0d7d532c95e3fd1e84196b
marcello33 <marcelloardizzone@hotmail.it>
2024-03-05 14:43:47 +0100

    chg: POS-2399: fix script with proper signersDump length

--- integration-tests/fund_ganache_accounts.sh
+++ integration-tests/fund_ganache_accounts.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 
-EthAmount='10'
-machine0='localhost'
+host='localhost'
 
 echo "Transferring funds from ganache account[0] to others..."
 
 signersFile="matic-cli/devnet/devnet/signer-dump.json"
+signersDump=$(jq . $signersFile)
+signersLength=$(jq '. | length' $signersFile)
 
-signerDump=$(cat $signersFile | jq -c '.')
+rootChainWeb3="http://${host}:9545"
 
-echo "src: $signersFile"
-echo "signerDump: $signerDump"
-
-rootChainWeb3="http://${machine0}:9545"
-
-for ((i = 1; i < ${#signerDump[@]}; i++)); do
-  to_address=$(echo "$signerDump" | jq -r ".[$i].address")
-  from_address=$(echo "$signerDump" | jq -r ".[0].address")
-  echo "Transferring funds from $from_address to $to_address"
-
-  txReceipt=$(curl -X POST --data '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"to":"'"$to_address"'","from":"'"$from_address"'","value":"'$EthAmount'"}],"id":1}' -H "Content-Type: application/json" $rootChainWeb3)
-
+for ((i = 1; i < signersLength; i++)); do
+  to_address=$(echo "$signersDump" | jq -r ".[$i].address")
+  from_address=$(echo "$signersDump" | jq -r ".[0].address")
+  txReceipt=$(curl $rootChainWeb3 -X POST --data '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"to":"'"$to_address"'","from":"'"$from_address"'","value":"0xDE0B6B3A7640000"}],"id":1}' -H "Content-Type: application/json")
   txHash=$(echo "$txReceipt" | jq -r '.result')
-
-  echo "📍Funds transferred from $from_address to $to_address with txHash $txHash"
+  echo "Funds transferred from $from_address to $to_address with txHash: $txHash"
 done
