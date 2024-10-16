418b1cd7206debd5e598d83462764bdbee6f7055
Angel Valkov <avalkov@polygon.technology>
2024-10-16 17:04:25 +0300

    Refactor

--- clerk/genesis.go
+++ clerk/genesis.go
@@ -25,5 +25,4 @@ func InitGenesis(ctx sdk.Context, keeper Keeper, data types.GenesisState) {
 // ExportGenesis returns a GenesisState for a given context and keeper.
 func ExportGenesis(ctx sdk.Context, keeper Keeper) types.GenesisState {
 	return types.NewGenesisState([]*types.EventRecord{}, keeper.GetRecordSequences(ctx))
-	// return types.NewGenesisState(keeper.GetAllEventRecords(ctx), keeper.GetRecordSequences(ctx))
 }
