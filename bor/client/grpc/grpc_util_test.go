0a52b7dd9374f1a845a553bc4a2693be00e5083e
marcello33 <marcelloardizzone@hotmail.it>
2024-11-13 16:08:42 +0100

    chg: parallel subtests

--- bor/client/grpc/grpc_util_test.go
+++ bor/client/grpc/grpc_util_test.go
@@ -44,6 +44,7 @@ func TestToBlockNumArg(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			result := ToBlockNumArg(tt.input)
 			assert.Equal(t, tt.expected, result)
 		})
