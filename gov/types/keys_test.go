b1fe58c6ac9f82fad8e3eddd0d5a543c50524dfe
marcello33 <marcelloardizzone@hotmail.it>
2024-12-05 11:56:36 +0100

    rm: useless tests

--- gov/types/keys_test.go
+++ gov/types/keys_test.go
@@ -5,9 +5,9 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
+	"github.com/tendermint/tendermint/crypto/secp256k1"
 
 	hmTypes "github.com/maticnetwork/heimdall/types"
-	"github.com/tendermint/tendermint/crypto/secp256k1"
 )
 
 var addr = hmTypes.BytesToHeimdallAddress(secp256k1.GenPrivKey().PubKey().Address())
@@ -38,34 +38,36 @@ func TestProposalKeys(t *testing.T) {
 
 func TestDepositKeys(t *testing.T) {
 
+	id := 1
+
 	key := DepositsKey(2)
 	proposalID := SplitProposalKey(key)
 	require.Equal(t, int(proposalID), 2)
 
-	key = DepositKey(2, addr)
+	key = DepositKey(2, hmTypes.ValidatorID(id))
 	proposalID, depositorAddr := SplitKeyDeposit(key)
 	require.Equal(t, int(proposalID), 2)
 	require.Equal(t, addr, depositorAddr)
 
 	// invalid key
-	addr2 := hmTypes.SampleHeimdallAddress("test1")
-	key = DepositKey(5, addr2)
+	key = DepositKey(5, hmTypes.ValidatorID(id))
 	require.Panics(t, func() { SplitKeyDeposit(key) })
 }
 
 func TestVoteKeys(t *testing.T) {
 
+	id := 1
+
 	key := VotesKey(2)
 	proposalID := SplitProposalKey(key)
 	require.Equal(t, int(proposalID), 2)
 
-	key = VoteKey(2, addr)
+	key = VoteKey(2, hmTypes.ValidatorID(id))
 	proposalID, voterAddr := SplitKeyDeposit(key)
 	require.Equal(t, int(proposalID), 2)
 	require.Equal(t, addr, voterAddr)
 
 	// invalid key
-	addr2 := hmTypes.SampleHeimdallAddress("test1")
-	key = VoteKey(5, addr2)
+	key = VoteKey(5, hmTypes.ValidatorID(id))
 	require.Panics(t, func() { SplitKeyVote(key) })
 }
