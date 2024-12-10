221e037f304f4976827d89f11b7038e91336ef18
Angel Valkov <avalkov@polygon.technology>
2024-12-10 09:42:14 +0200

    set non-zero height for antevorta

--- helper/config.go
+++ helper/config.go
@@ -436,7 +436,7 @@ func InitHeimdallConfigWith(homeDir string, heimdallConfigFileFromFLag string) {
 		newHexToStringAlgoHeight = 0
 		aalborgHeight = 0
 		jorvikHeight = 0
-		antevortaHeight = 0
+		antevortaHeight = 10000
 	}
 }
 
