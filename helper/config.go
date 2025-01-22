ed61e75f3bb93a36b927045a074f4ebd769e5c40
Angel Valkov <avalkov@polygon.technology>
2025-01-22 12:11:15 +0200

    Set test heights

--- helper/config.go
+++ helper/config.go
@@ -435,8 +435,8 @@ func InitHeimdallConfigWith(homeDir string, heimdallConfigFileFromFLag string) {
 		spanOverrideHeight = 0
 		newHexToStringAlgoHeight = 0
 		aalborgHeight = 0
-		jorvikHeight = 0
-		danelawHeight = 0
+		jorvikHeight = 20000
+		danelawHeight = 20000
 	}
 }
 
