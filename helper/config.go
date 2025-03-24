cc9beea08013cfdf1d94bbba6e457691502766c9
marcello33 <marcelloardizzone@hotmail.it>
2025-03-24 17:33:29 +0100

    reduce halt height to 20

--- helper/config.go
+++ helper/config.go
@@ -244,7 +244,7 @@ var jorvikHeight int64 = 0
 
 var danelawHeight int64 = 0
 
-var apocalypseHeight int64 = 100
+var apocalypseHeight int64 = 20
 
 type ChainManagerAddressMigration struct {
 	MaticTokenAddress     hmTypes.HeimdallAddress
