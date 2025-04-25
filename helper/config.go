5d045ffefdc96c27611b036f37c19a61efea6108
Angel Valkov <avalkov@polygon.technology>
2025-04-25 16:10:48 +0300

    Lower halt height

--- helper/config.go
+++ helper/config.go
@@ -244,7 +244,7 @@ var jorvikHeight int64 = 0
 
 var danelawHeight int64 = 0
 
-var apocalypseHeight int64 = 20000000
+var apocalypseHeight int64 = 2000
 
 type ChainManagerAddressMigration struct {
 	MaticTokenAddress     hmTypes.HeimdallAddress
