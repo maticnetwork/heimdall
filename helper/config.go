89886cac8c81db4af306e37f1b832a547e13c343
Raneet Debnath <raneetdebnath10@gmail.com>
2025-05-21 17:15:42 +0530

    helper: bump apocalypseHeight for tests

--- helper/config.go
+++ helper/config.go
@@ -244,7 +244,7 @@ var jorvikHeight int64 = 0
 
 var danelawHeight int64 = 0
 
-var apocalypseHeight int64 = 200
+var apocalypseHeight int64 = 400
 
 type ChainManagerAddressMigration struct {
 	MaticTokenAddress     hmTypes.HeimdallAddress
